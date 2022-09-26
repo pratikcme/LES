a45cf602b65c16abe8fd0d3f2931126f9da45cf7
shahidCME <sahid.cmexpertise@gmail.com>
2022-09-26 11:17:34 +0530

    Update Order.php

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -312,8 +312,6 @@ class Order extends Vendor_Controller
   public function user_sell_report(){
     $data['table_js'] = ['user_sell_report.js'];
     $data['user_sell_report'] = $this->this_model->user_sell_report();
-    lq();
-
     // dd($data['user_sell_report']);
     $this->load->view('user_sell_report',$data);
   }
