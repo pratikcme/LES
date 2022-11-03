5b2cdabdb21effd38ebbcb2b3f4bd8a07ebd4fed
shahidCME <sahid.cmexpertise@gmail.com>
2022-11-03 15:30:49 +0530

    Update Order_model.php

--- application/models/frontend/Order_model.php
+++ application/models/frontend/Order_model.php
@@ -196,8 +196,8 @@ Class Order_model extends My_model{
        
 
         $my_order_result = $this->product_model->getMyCartOrder();
-        // echo "<pre>";
-        // print_r($my_order_result);die;
+        echo "<pre>";
+        print_r($my_order_result);die;
 
         $promocode_amount = 0;
 
