9d75ee897a2048112eff33d8b4444ab5cc86e342
shahidCME <sahid.cmexpertise@gmail.com>
2023-02-24 19:05:00 +0530

    Revert "Update Order_model.php"
    This reverts commit 68a7b6139e64cebf3ccd023b87e2dfe3ca1143d3.

--- application/models/frontend/Order_model.php
+++ application/models/frontend/Order_model.php
@@ -212,18 +212,23 @@ class Order_model extends My_model
             $my_order_result = $this->product_model->getMyCartOrder();
             // echo "<pre>";
             // print_r($my_order_result);die;
-            $promocode_amount = 0;
 
-            if(isset($promocode) && $promocode !=''){
-                      unset($data);
-                      $data['where'] = ['branch_id'=>$branch_id,'name'=>$promocode];
-                      $data['table'] = TABLE_PROMOCODE;
-                      $promocodeData = $this->selectRecords($data);
-  
-                  if(!empty($promocodeData)){
-                      $promocode_amount =  ($total_price / 100 ) * $promocodeData[0]->percentage;
-                  }
+                    if(!empty($promocodeData)){
+                        $promocode_amount =  ($total_price / 100 ) * $promocodeData[0]->percentage;
+                    }
+                
+                }
+
+        if(!empty($my_order_result)){
+            foreach ($my_order_result as $my_order) {
+                $var_id = $my_order->product_weight_id;
+                $qnt = $my_order->quantity;
+                $updatedQTY = $this->check_udpate_quantity($var_id,$qnt,$user_id);
+                if(!$updatedQTY){
+                    continue;
+                }
             }
+
 
             if (!empty($my_order_result)) {
                 foreach ($my_order_result as $my_order) {
