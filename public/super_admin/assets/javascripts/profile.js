481e60156ae9aeddc0cad801444d62c851d0de59
shahidCME <sahid.cmexpertise@gmail.com>
2023-02-28 15:12:09 +0530

    Update profile.js

--- public/super_admin/assets/javascripts/profile.js
+++ public/super_admin/assets/javascripts/profile.js
@@ -1,50 +1,61 @@
 var USERPROFILE = function () {
-    $(document).ready(function(){
-                $('.alert').fadeOut(5000);
-            });
+  $(document).ready(function () {
+    $('.alert').fadeOut(5000);
+  });
 
-    var HandleProfile = function () {
-    
-         $('#ProfileForm').validate({
-        rules:{
-        	company_name:{ required:true},
-        	email:{ required:true,
-        			email:true 
-        		},
-            password:{ required:true,
-                     },
-            new_password:{required:true,
-            	},
-            confirm_password: {
-		            required: true,
-		            equalTo: "#new_password"
-        }},
-        messages:{
-        	company_name:{ required : "Please enter company name"},
-        	email:{ required : "Please enter email",
-        			email : 'Please enter valid email address'
-        		  },
-            password:{required : "Please enter old password",
-                      minlength:   "Please enter minmum 6 digit password",
-                      maxlength:   "Your Password must be less than 10 digit"
-                    },
-            new_password:{required : "Please enter new password",
-        			  minlength : "Please enter minmum 6 digit password",
-        			  maxlength : "Your Password must be less than 10 digit"
-        			},
-            confirm_password:{ required : "Please enter confirm password",
-            		       minlength : "Please enter minmum 6 digit password",
-            		       maxlength : "Your Password must be less than 10 digit",
-            		       equalTo : "Your password does not match"
-            	 }
+  var HandleProfile = function () {
+
+    $('#ProfileForm').validate({
+      rules: {
+        company_name: {
+          required: true,
+          maxlength: 25
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        password: {
+          required: true,
+        },
+        new_password: {
+          required: true,
+        },
+        confirm_password: {
+          required: true,
+          equalTo: "#new_password"
         }
-       });
-	}
-    
-    return {
-        //main function to initiate the module
-        init: function () {
-           HandleProfile();
+      },
+      messages: {
+        company_name: { required: "Please enter company name" },
+        email: {
+          required: "Please enter email",
+          email: 'Please enter valid email address'
+        },
+        password: {
+          required: "Please enter old password",
+          minlength: "Please enter minmum 6 digit password",
+          maxlength: "Your Password must be less than 10 digit"
+        },
+        new_password: {
+          required: "Please enter new password",
+          minlength: "Please enter minmum 6 digit password",
+          maxlength: "Your Password must be less than 10 digit"
+        },
+        confirm_password: {
+          required: "Please enter confirm password",
+          minlength: "Please enter minmum 6 digit password",
+          maxlength: "Your Password must be less than 10 digit",
+          equalTo: "Your password does not match"
         }
-    };
+      }
+    });
+  }
+
+  return {
+    //main function to initiate the module
+    init: function () {
+      HandleProfile();
+    }
+  };
 }();
