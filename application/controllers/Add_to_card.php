78d5ca27706b2be67e9a2710b54cdc9683fa8151
shahidCME <sahid.cmexpertise@gmail.com>
2023-02-28 17:48:39 +0530

    Update Add_to_card.php

--- application/controllers/Add_to_card.php
+++ application/controllers/Add_to_card.php
@@ -47,8 +47,8 @@ class Add_to_card extends User_Controller
 		}
 
 		if (!empty($result)) {
-			// dd($result);
-			if (!empty($isShow) && $isShow[0]->display_price_with_gst == '0') {
+			dd($result);
+			if (!empty($isShow) && $isShow[0]->display_price_with_gst == '1') {
 				$result[0]->discount_price = $result[0]->without_gst_price;
 			}
 
