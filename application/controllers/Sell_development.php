57700bd648cabb6df5a03b4a913c46be1a549531
Dipesh Khedkar <dipesh.cmexpertise@gmail.com>
2023-03-10 18:40:05 +0530

    DIpesh Sell HIstory Changed

--- application/controllers/Sell_development.php
+++ application/controllers/Sell_development.php
@@ -487,40 +487,54 @@ class Sell_development extends Vendor_Controller
             $re = $this->this_model->viewOrderDetails($this->input->post());
             $o_detail = '';
 
+            $html = '';
+            $amount = 0;
+
+            if ($re['orderInfo'][0]->promocode_used == 1) {
+                $amount = $this->this_model->getPromocodeAmount($re['orderInfo'][0]->order_id);
+                $html = '<li>
+                <div>
+                    <h6>Promocode Discount(%)</h6>
+                    <h6> - ' . '(' . $amount->percentage . '%)' . numberFormat($amount->amount) . '</h6>
+                </div>
+            </li>';
+            }
+
+            //$shoppingDiscount = $this->this_model->checkShoppingBasedDiscount($re['orderInfo'][0]->total);
+
+            $shopping_based_html = '';
+            if ($re['orderInfo'][0]->shopping_amount_based_discount > 0) {
+                $shopping_based_html  = '<li>
+                <div>
+                    <h6>Cart Amount Based Discount(%)</h6>
+                    <h6> - ' . '(' . 10 . '%)' . numberFormat($re['orderInfo'][0]->shopping_amount_based_discount) . '</h6>
+                </div>
+            </li>';
+            }
+
             foreach ($re['order_details'] as $key => $v) {
                 $o_detail .= '<tr>
-    <td>' . $v->name . '</td>
-    <td>' . $v->quantity . '</td>
-    <td>' . $v->discount . '</td>
-    <td>' . $v->calculation_price . '</td>
-</tr>';
+                    <td>' . $v->name . '</td>
+                    <td>' . $v->quantity . '</td>
+                    <td>' . $v->discount . '</td>
+                    <td>' . $v->calculation_price . '</td>
+                    </tr>';
             }
             $o_info = '<li>
-    <div>
-        <h6>Subtotal </h6>
-        <h6>' . $re['orderInfo'][0]->total . '</h6>
-    </div>
-</li>
-<li>
-    <div>
-        <h6>Discount(%)</h6>
-        <h6>' . $re['orderInfo'][0]->order_discount . '</h6>
-    </div>
-</li>
-<li>
-    <div>
-        <h6>Discount Price </h6>
-        <h6>' . $re['orderInfo'][0]->total_saving . '</h6>
-    </div>
-</li>
-<li>
-    <div>
-        <h6>Total </h6>
-        <h6>' . numberFormat($re['orderInfo'][0]->payable_amount) . '</h6>
-    </div>
-</li>';
+                        <div>
+                            <h6>Subtotal </h6>
+                            <h6>' . $re['orderInfo'][0]->total . '</h6>
+                        </div>
+                    </li>
+                    ' . $html . '' . $shopping_based_html  . '
+                    <li>
+                        <div>
+                            <h6>Total </h6>
+                            <h6>' . numberFormat($re['orderInfo'][0]->payable_amount) . '</h6>
+                        </div>
+                    </li>';
             $date = ' <h4></h4>
-<h5> <span>Date : </span> ' . date('d -m- Y', $re['orderInfo'][0]->dt_added) . ' </h5>';
+                    <h5> <span>Date : </span> ' . date('d -m- Y', $re['orderInfo'][0]->dt_added) . ' </h5>';
 
             echo json_encode(['o_detail' => $o_detail, 'o_info' => $o_info, 'date' => $date]);
         }
