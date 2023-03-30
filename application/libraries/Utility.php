0ff293d7665b144609c08f1dd69c4144a5304ee7
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-03-30 10:35:01 +0530

    Update Utility.php
    remove

--- application/libraries/Utility.php
+++ application/libraries/Utility.php
@@ -499,9 +499,7 @@ class Utility
         $staff_bandle_id = $result[0]->staff_bandle_id;
         $delivery_bandle_id = $result[0]->delivery_bandle_id;
 
-        
-        $deviceId = "$deviceIds['device_id']";
-
+        $deviceId = $deviceIds['device_id'];
         $msg = $msg['message'];
         
 
