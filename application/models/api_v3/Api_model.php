bff872e25f0374c6079398095fdddc15de635956
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-04-03 16:38:13 +0530

    api_model.php ,index.php
    index php change mode and solve quaery

--- application/models/api_v3/Api_model.php
+++ application/models/api_v3/Api_model.php
@@ -1669,7 +1669,7 @@ class Api_model extends My_model {
     }
 
     public function checkShoppingBasedDiscount($cartTotal,$branch_id){
-        $query = $this->db->query('SELECT *,('.$cartTotal.' - cart_amount) AS CA FROM `amount_based_discount` where status = "1" branch_id = '.$branch_id.' HAVING CA > 0 ORDER BY CA ASC LIMIT 1');
+        $query = $this->db->query('SELECT *,('.$cartTotal.' - cart_amount) AS CA FROM `amount_based_discount` where status = "1" AND branch_id = '.$branch_id.' HAVING CA > 0 ORDER BY CA ASC LIMIT 1');
         $re = $query->result();
         // lq();
         return $re;
