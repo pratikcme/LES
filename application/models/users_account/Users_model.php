50795bce6613bf872c072f8e0d553d798e692d45
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-04 16:48:20 +0530

    Update Users_model.php

--- application/models/users_account/Users_model.php
+++ application/models/users_account/Users_model.php
@@ -9,8 +9,8 @@ class Users_model extends My_model
         if (isset($_FILES['profileimage']) && $_FILES['profileimage']['error'] == 0) {
 
             $UploadPath = "public/images/" . $this->folder . "user_profile/";
-            echo $UploadPath;die;
             $uploadImage =  upload_single_image($_FILES, 'uprofile', $UploadPath);
+            dd($uploadImage);
             $uploadImage = $uploadImage['data']['file_name'];
             $data['update']['profileimage'] =  $uploadImage;
 
