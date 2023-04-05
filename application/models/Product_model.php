e267c5b701e421a4eda7f66bf971034a06d3f3b1
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-04-05 14:58:02 +0530

    Update Product_model.php

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -565,6 +565,7 @@ class product_model extends My_model
         $limited_stock = $_POST['limited_stock'];
 
         $discount_price_cal = (($price * $discount_per) / 100);
+        echo $discount_price_cal;exit;
         $discount_price = number_format((float)$discount_price_cal, 2, '.', '');
         $final_discount_price = number_format((float)$price - $discount_price, 2, '.', '');
         // $whole = floor($unit);      
