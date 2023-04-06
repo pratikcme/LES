0065d457b76e1ef6aabd5cc9b05f6376b99d7373
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-04-06 13:02:35 +0530

    Update admin_sidebar.php
    change url offf web logo in admin_sidebar

--- application/views/admin_sidebar.php
+++ application/views/admin_sidebar.php
@@ -129,7 +129,7 @@
             </a>
         </li>
         <li>
-            <a class="<?php if ($this->uri->segment(1) == 'web_setting' && $this->uri->segment(3) == 'web_logo' || $this->uri->segment(2) == 'add') { ?> active <?php } ?>" href="<?php echo base_url() . 'admins/about/about_section_two'; ?>">
+            <a class="<?php if ($this->uri->segment(1) == 'web_setting' && $this->uri->segment(3) == 'web_logo'  { ?> active <?php } ?>" href="<?php echo base_url() . 'web_setting/web_logo'; ?>">
                 <i class="fa fa-picture-o"></i>
                 <span>Web Logo</span>
             </a>
