4e5dc3faa59c03defbb39bf661e3378f9672ad77
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-06 19:09:36 +0530

    Update checkout.js

--- public/upbasket/assets/javascript/checkout.js
+++ public/upbasket/assets/javascript/checkout.js
@@ -172,6 +172,7 @@ var CHECKOUT = (function () {
     // alert('All services are disabled');
     // return false;
     if (paymentOption == 0) {
+      return false;
       $(".loader-main").removeClass("d-none");
       CheckSelfPickUpEnable();
       $.ajax({
