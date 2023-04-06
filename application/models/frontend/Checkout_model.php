fb7d6d6f0444cc0045a99ee70bedda157b9d48a9
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-06 19:16:02 +0530

    Update Checkout_model.php

--- application/models/frontend/Checkout_model.php
+++ application/models/frontend/Checkout_model.php
@@ -74,8 +74,8 @@ class Checkout_model extends My_model
         // dd($get_vandor_address);
         $getkm = $this->circle_distance($lat, $long, $get_vandor_address[0]->latitude, $get_vandor_address[0]->longitude);
         $getkm = round($getkm);
-
-        print_r($getkm);die;
+        lq();
+        // print_r($getkm);die;
         unset($data);
         $data['select'] = ['id'];
         $data['table'] = 'delivery_charge';
