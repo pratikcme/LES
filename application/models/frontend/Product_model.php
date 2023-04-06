596d05046745510b255e1bf3d29741f33963b274
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-04-06 16:41:25 +0530

    Update Product_model.php

--- application/models/frontend/Product_model.php
+++ application/models/frontend/Product_model.php
@@ -262,8 +262,7 @@ class Product_model extends My_model
 		$this->load->model('api_v3/common_model', 'co_model');
 		$isShow = $this->co_model->checkpPriceShowWithGstOrwithoutGst($this->session->userdata('vendor_id'));
 
-		print_r($postdata);
-		die;
+
 		if (isset($postdata['slider'])) {
 			$postdata['getCatByURL'] = $this->utility->safe_b64encode($postdata['getCatByURL']);
 		}
