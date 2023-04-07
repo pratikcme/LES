2aec42d91a15683cf8c767f37667bdbedcc2a8a1
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-07 15:17:58 +0530

    Update footer.php

--- application/views/upbasket/include/footer.php
+++ application/views/upbasket/include/footer.php
@@ -16,8 +16,9 @@
   <input type="hidden" name="session_vendor_id" id="session_vendor_id" value="<?=(isset($_SESSION['branch_id'])) ? $_SESSION["branch_id"] : '' ?>">
 <!-- -----jquary-min----- -->
 <script src="<?=$this->theme_base_url.'/assets/js/jquery.min.js'?>"></script>
+<!-- bootstrap-datepicker-js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
 
 <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
 
@@ -29,8 +30,6 @@
 
 <!-- Latest compiled JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-<!-- bootstrap-datepicker-js -->
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script> -->
 
 <script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/164071/Drift.min.js'></script>
 
