71e5cecd6dae2c995cbd86794e4f8b07f2be4ace
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-07 15:31:20 +0530

    Update checkout.js

--- public/upbasket/assets/javascript/checkout.js
+++ public/upbasket/assets/javascript/checkout.js
@@ -1,10 +1,5 @@
 
-$('#datepicker').datepicker({
-  // inline: true,
-  // firstDay: 1,
-  // showOtherMonths: true,
-  // dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-})
+
 var CHECKOUT = (function () {
   var url = $("#url").val();
   var address_checked = false;
