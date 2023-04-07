2126465a7320421b7a681e7e6f7864136c151de1
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-07 15:06:39 +0530

    ff

--- application/views/upbasket/checkout.php
+++ application/views/upbasket/checkout.php
@@ -497,18 +497,3 @@ function onScriptLoad(txnToken, orderId, amount) {
     </div>
    </div> 
   </div>
-
-  <script>
-
-    
-    
-    
-$(document).ready(function () {
-  $('#calendar').datepicker({
-    inline: true,
-    firstDay: 1,
-    showOtherMonths: true,
-    dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-  })
-});
-  </script>
