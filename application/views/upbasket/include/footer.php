b02afa2548c3e4c7ac2e5cd4f13b14551d8bd352
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-07 15:41:03 +0530

    Update footer.php

--- application/views/upbasket/include/footer.php
+++ application/views/upbasket/include/footer.php
@@ -226,9 +226,7 @@
           setTimeout(function () { $('#registered').hide(); }, 6000);
             
       </script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-    <!-- <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> -->
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/additional-methods.js" type="text/javascript"></script> 
     <script src="<?=base_url();?>public/<?=$_SESSION['template_name']?>/assets/javascript/common.js?v=<?=js_version?>"></script>
