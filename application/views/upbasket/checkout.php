54e09eb8d1faed6174d4c148d63b93565ed86ac2
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-10 14:26:33 +0530

    Update checkout.php

--- application/views/upbasket/checkout.php
+++ application/views/upbasket/checkout.php
@@ -106,17 +106,7 @@
                   </div>
                 </div>
                 <?php if(isset($_SESSION['isSelfPickup']) && $_SESSION['isSelfPickup'] == '1'){?>
-                <div class="main-accordion">
-                  <div class="accordion-heading"><?=$this->lang->line('Pickup Address')?></div>
-                  <div class="accordion-content accordion-content-3">
-                      <div class="address-wrapper">
-                          <div class="ship-check text-end"></div>
-                          <div class="address-text mt-3">
-                            <p><?=$selfPickupTimeChart[0]->selfPickupOpenClosingTiming?></p>
-                          </div>
-                      </div>
-                  </div>
-                </div>
+               
                 <?php }else{ ?>
                 <div class="main-accordion">
                   <div class="accordion-heading">Shipping address</div>
