b859991f99046a305fa4a3dcf63b8102c1f1e529
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-10 12:54:29 +0530

    Update checkout.php

--- application/views/upbasket/checkout.php
+++ application/views/upbasket/checkout.php
@@ -303,6 +303,7 @@
                </div>
               </div>
           </div>
+        </div>
         <div class="col-xxl-5 col-xl-5 col-lg-6 col-md-6">
             <div class="checkout-order-detils">
                 <div class="mydiv-wrapper">
