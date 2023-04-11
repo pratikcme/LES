8923381e70833a366093ee9236126027843a6faa
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-11 14:39:12 +0530

    loader css update

--- application/views/upbasket/layout.php
+++ application/views/upbasket/layout.php
@@ -12,7 +12,14 @@
 <!-- <body class="rtl" dir="rtl"> -->
 
 <body>
-  <section class="loader-main"></section>
+  <section class="loader-main">
+    <div class="loader-wrapper">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+    </div>
+  </section>
   <header>
     <?php $this->load->view($_SESSION['template_name'] . '/include/body_header'); ?>
   </header>
