b47a1571e6725686652ae72e0d825b58afaebc18
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-11 15:31:53 +0530

    Update Checkout.php

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -111,7 +111,7 @@ class Checkout extends User_Controller
         $data['calc_shiping'] = 'notInRange';
       }
     }
-    dd($_SESSION);
+    // dd($_SESSION);
     // dd($data['calc_shiping']);
     if ($data['calc_shiping'] == 'notInRange'){
       $calc_shiping = 0;
