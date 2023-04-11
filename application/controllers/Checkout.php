e56f417e528d85af5da06971e99e0ad5756ef3da
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-11 15:34:14 +0530

    Update Checkout.php

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -106,6 +106,7 @@ class Checkout extends User_Controller
     $calc_shiping = 0;
 
     if (!isset($_SESSION['isSelfPickup']) || $_SESSION['isSelfPickup'] == '0') {
+      echo '212121';
       $calc_shiping = $this->this_model->getDeliveryCharge($userLat, $userLong, $this->session->userdata('branch_id'), getMycartSubtotal());
       if($calc_shiping =='0.00'){
         echo "ddd";
