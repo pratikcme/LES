b77fd5d7c29f9b9a5dcf8735b947daed23fd7713
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-11 16:00:28 +0530

    Revert "Update Checkout.php"
    This reverts commit b63c3d0d86190de8ebe7c8b06e63f14852557b9b.

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -111,9 +111,8 @@ class Checkout extends User_Controller
 
     if (!isset($_SESSION['isSelfPickup']) || $_SESSION['isSelfPickup'] == '0') {
       $calc_shiping = $this->this_model->getDeliveryCharge($userLat, $userLong, $this->session->userdata('branch_id'), getMycartSubtotal());
-      if($calc_shiping =='0.00'){
-        $data['calc_shiping'] = 'notInRange';
-      }
+      $data['calc_shiping'] = $calc_shiping;
+
       // dd($data['calc_shiping']);
     }
 
