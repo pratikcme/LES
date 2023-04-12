5ae96b71083e485bfdba58da71b0192347dc5476
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-12 14:24:38 +0530

    Update Checkout.php

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -645,10 +645,10 @@ class Checkout extends User_Controller
     if (isset($getActivePaymentMethod[0]->type) && $getActivePaymentMethod[0]->type == 1) { // razor payment
 
       $api = new Api($publish_key, $scret_key);
-      $amt = $getMycartSubtotal + $calc_shiping;
+      $amt = $getMycartSubtotal + $calc_shiping; 
       $razorpayOrder = $api->order->create(array(
         'receipt' => rand(),
-        'amount' => $amt * 100, // 2000 rupees in paise
+        'amount' => (int)$amt * 100, // 2000 rupees in paise
         'currency' => $currency_code,
         'payment_capture' => 1 // auto capture
       ));
