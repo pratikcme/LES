6cd657c9184acab6a0eef02e501f8bd7da8278aa
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-12 15:01:51 +0530

    Update Checkout_model.php

--- application/models/frontend/Checkout_model.php
+++ application/models/frontend/Checkout_model.php
@@ -459,7 +459,7 @@ class Checkout_model extends My_model
         }
         $sub_total = number_format((float)($getMycartSubtotal - $discountValue), 2, '.', '');
         $total_price = number_format((float)$sub_total, 2, '.', '');
-        var_dump($total_price);
+
         if (empty($promocode)) {
             $response["success"] = 0;
             $response["message"] = "No Promocode Found";
@@ -524,7 +524,7 @@ class Checkout_model extends My_model
         $response["success"] = 1;
         $response["message"] = "Promocode applied";
         $response["data"] = $calculate;
-        $response["orderAmount"] = floatval($total_price);
+        $response["orderAmount"] = $total_price;
         $response["withoutPromo"] = totalSaving() + $discountValue;
         return $response;
     }
