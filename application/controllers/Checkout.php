46cf5080a2b30019691f5b81f15fde033b734d64
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-12 13:14:02 +0530

    Update Checkout.php

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -399,6 +399,7 @@ class Checkout extends User_Controller
       $calc_shiping = 0;
       if (!isset($_SESSION['isSelfPickup']) || $_SESSION['isSelfPickup'] == '0') {
         $calc_shiping = $this->this_model->getDeliveryCharge($userLat, $userLong, $this->session->userdata('branch_id'), getMycartSubtotal());
+      
       }
       // this call for currency code
       $currency = $this->this_model->checkCurrencyCode();
@@ -631,12 +632,14 @@ class Checkout extends User_Controller
     $otpForSelfPickup = '';
     $calc_shiping = '0';
     if (!isset($_SESSION['isSelfPickup']) || $_SESSION['isSelfPickup'] == '0') {
-      $calc_shiping =
-        $this->this_model->getDeliveryCharge($userLat, $userLong, $this->session->userdata('branch_id'), getMycartSubtotal());
+      $calc_shiping = $this->this_model->getDeliveryCharge($userLat, $userLong, $this->session->userdata('branch_id'), getMycartSubtotal());
+      if($calc_shiping =='0.00'){
+        $calc_shiping = 'notInRange';
+      }
     }
 
     if ($calc_shiping == 'notInRange') {
-      $calc_shiping = 0;
+        $calc_shiping = 0;
     }
 
     if (isset($getActivePaymentMethod[0]->type) && $getActivePaymentMethod[0]->type == 1) { // razor payment
