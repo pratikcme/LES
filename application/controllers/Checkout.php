99ed2c1ed2b49a014978337daa3f8b5023d8d057
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-12 14:12:54 +0530

    Update Checkout.php

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -595,7 +595,7 @@ class Checkout extends User_Controller
         die;
       }
     }
-
+    dd($getMycartSubtotal);die;
     $data['response'] = '1';
     $getActivePaymentMethod = $this->this_model->ActivePaymentMethod();
     $data['payment_option'] = $getActivePaymentMethod[0]->type;
@@ -641,7 +641,7 @@ class Checkout extends User_Controller
     if ($calc_shiping == 'notInRange') {
         $calc_shiping = 0;
     }
-    
+
     if (isset($getActivePaymentMethod[0]->type) && $getActivePaymentMethod[0]->type == 1) { // razor payment
 
       $api = new Api($publish_key, $scret_key);
