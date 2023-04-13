d5248dbb832d308b204724c80f06b42debdf2891
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-13 15:09:17 +0530

    Update order_success.php

--- application/views/upbasket/order_success.php
+++ application/views/upbasket/order_success.php
@@ -2,38 +2,28 @@
   <div class="container">
   <?php if($status == 1){ ?>
   <!-- The Modal -->
-    <div class="modal" id="order_success">
-      <div class="modal-dialog">
-        <div class="modal-content">
-        
-          <!-- Modal Header -->
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
+    <div id="order_success" class="modal">
+        <div class="container"> 
+          <div class="modal-content">
+            <span class="close"><i class="fa-regular fa-circle-xmark"></i></span>
+            <div class="login-page">
+              <div class="container">
+                <img src="<?=$this->theme_base_url?>/assets/images/login-center-border-img.png" alt="" class="login-center-border-img">
+                  <div class="center-img">
+                      <img src="<?=$this->theme_base_url?>/assets/images/login-center-img.png" alt="">
+                  </div>
+                  <h2>Thank you.</h2>
+                  <h3>Your order has been received</h3>
+                  <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
+                  <h5 id="orderId"></h5>
+                  <div class="continue-btn">
+                    <a href="<?=base_url().'home'?>"><?=$this->lang->line('continue shopping')?></a>
+                  </div>
+              </div>
+            </div>
           </div>
-          
-          <!-- Modal body -->
-          <div class="modal-body text-center">
-              <div>
-                  <h3>thanks for shopping</h3>
-              </div>
-              <div class="my-3">
-                  <i class="fa fa-shopping-bag" aria-hidden="true"></i>
-              </div>
-              <!-- <div class="my-3">
-                  <img src="assets/images/bag.png" class="bag" alt="">
-              </div> -->
-              <div>
-                  <h5>order placed successfully</h5>
-                  <p id="orderId"><?=$order_number?></p>
-              </div>
-              <div>
-                  <a href="<?=base_url().'home'?>" class="shopping_btn">continue shopping</a>
-              </div>
-          </div>
-          
-        </div>
+        </div> 
       </div>
-    </div>
   <?php } ?>
   <?php if($status == '0'){ ?>
     <!-- The Modal -->
