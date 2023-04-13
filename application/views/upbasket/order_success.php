4957c2b4e46f1d365cf7b4d7da1dd0ac56b077db
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-13 19:38:00 +0530

    Update order_success.php

--- application/views/upbasket/order_success.php
+++ application/views/upbasket/order_success.php
@@ -27,11 +27,35 @@
   <?php } ?>
   <?php if($status == '0'){ ?>
     <!-- The Modal -->
+    <!-- =============Cancel order popup=========== -->
+    <div id="myMopayment_faildal" class="modal">
+      <div class="container">
+        <div class="modal-content">
+          <span class="close"><i class="fa-regular fa-circle-xmark"></i></span>
+          <div class="login-page">
+            <div class="container">
+              <img src="<?=$this->theme_base_url?>/assets/images/login-center-border-img.png" alt="" class="login-center-border-img">
+              <div class="center-img">
+                <img src="<?=$this->theme_base_url?>/assets/images/login-center-img.png" alt="">
+              </div>
+              <h2><?=$this->lang->line('Payment Failed')?></h2>
+              <!-- <h3>Sorry, Your Payment has been Canceled</h3> -->
+              <p><?=$message?></p>
+              
+              <div class="continue-btn">
+                <a href="<?=base_url().'home'?>">Try Again</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+<!-- 
     <div class="modal" id="payment_fail">
       <div class="modal-dialog">
         <div class="modal-content">
         
-          <!-- Modal body -->
+        Modal body 
           <div class="modal-body text-center">
               <div>
                   <h3>payment failed</h3>
@@ -49,7 +73,7 @@
           
         </div>
       </div>
-    </div>    
+    </div>     -->
    <?php } ?>
     
     </div>
