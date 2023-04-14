8f9dcafef3d6b5aa328b7f03d332b8d85a8629bc
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-14 16:41:58 +0530

    Update addProduct.js

--- public/upbasket/assets/javascript/addProduct.js
+++ public/upbasket/assets/javascript/addProduct.js
@@ -280,8 +280,8 @@ var ADDPRODUCT = function () {
             // $('.orginal-price').css('display', '');
           }
           var disNwislist = '<span class="discnt ' + (output.discount_per > 0) ? '' : 'd-none' + '">' + output.discount_per + '% off</span>< div class="pro-hearticon wishlist-icon" data-product_id="' + output.product_id + '" data-product_weight_id ="' + output.product_variant_id + '">< i class="fa-regular fa-heart ' + output.isInWishList + '"></i ></div >';
-          alert(disNwislist);
-          $('#zoom_image').before(disNwislist);
+          alert(output.isInWishList);
+          $('#zoom_image').before(output.isInWishList);
 
           if (output.varient_quantity > 25) {
             $('#is_aval_stock').html(stockMessage1);
