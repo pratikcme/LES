b7beda04956d25a2f3983111557eb005b85abd33
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-14 16:01:34 +0530

    Update addProduct.js

--- public/upbasket/assets/javascript/addProduct.js
+++ public/upbasket/assets/javascript/addProduct.js
@@ -266,8 +266,10 @@ var ADDPRODUCT = function () {
           $('#review_section').html(output.reviewSection);
           $('#starRatting').html(output.upbasket_starHtml);
           // siteCurrency + ' ' + output.discount_price ;
-          alert('<span><strike></strike></span>');
-          $('#dynamic_price').html(siteCurrency + ' ' + output.discount_price + '<span><strike>' + (output.discount_per > 0) ? siteCurrency + ' ' + output.product_price : + '' + '</strike></span>');
+          alert('<span><strike>15</strike></span>');
+          $('#dynamic_price').html(siteCurrency + ' ' + output.discount_price + '<span><strike>15</strike></span>');
+
+          // $('#dynamic_price').html(siteCurrency + ' ' + output.discount_price + '<span><strike>' + (output.discount_per > 0) ? siteCurrency + ' ' + output.product_price : + '' + '</strike></span>');
           var imageFolder = $('#imageFolder').val();
           var images = output.images;
 
