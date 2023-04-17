86ee007fe030d5256090f55c3e669d665309d333
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-17 16:40:54 +0530

    ds

--- application/views/beauty/account/login.php
+++ application/views/beauty/account/login.php
@@ -1,51 +1,66 @@
-<!-- -----login-section----- -->
-  <div class="login-section section">
+<!-- ----hero-section--- -->
+<section class="hero-section login-section common-banner-bg">
     <div class="container">
         <div class="row">
-          <div class="col-xxl-6 col-lg-6">
-            <!-- --------login-left-content---- -->
-            <div class="login-left-content">
-              <h2 class="title">Welcome back</h2>
-              <p class="pera">Welcome back! Enter your credentails to acess your account.</p>
-              
-              <!-- ---------login-deails-wrappper----- -->
-              <div class="login-deatils-wrapper">
-                  <form id="LoginForm" action="<?=base_url().'login'?>" method="post">
-                    <div class="mb-3">
-                      <label for="exampleInputEmail1" class="form-label">Email</label>
-                      <input type="text" name="email" class="form-control"  placeholder="Email*">
-                    </div>
-                    <div class="mb-3">
-                      <label for="exampleInputPassword1" class="form-label">Password</label>
-                      <input type="password" name="password" class="form-control" id="password" placeholder="password*">
-                    </div>
-                    <a href="<?=base_url().'login/forget_password'?>" class="forgot-password-text">Forgot Password?</a>
-
-                    <div class="sign-in-btn">
-                      <!-- <button type="submit" class="signin-btn-green">Sign Up</button> -->
-                      <input class="common-input-btn" type="submit" id="btnSubmit"  value="<?=$this->lang->line('Sign In')?>"/>
-                    </div> 
-                    <p>Or, log in with your socials</p>
-                    <div class="log-in-btn">
-                      <a href="<?=$googleUrl?>" class="google-btn"><span><i class="fa-brands fa-google"></i></span>Login with Google</a>
-                      <a href="<?=base_url().'login/fb_login'?>" class="facebook-btn"><span><i class="fa-brands fa-facebook"></i></span>Login with Facebook</a>
-                    </div>
-                    <h3>Dont’ hvae an account? <a href="<?=base_url().'register'?>"><?=$this->lang->line('Sign up')?></a></h3>
-                  </form>
-              </div>
-          </div>
-          </div>
-
-          <!-- ------login-right-image-- -->
-          <div class="login-images col-xxl-6 col-lg-6">
-            <img src="<?=$this->theme_base_url?>/assets/images/login-say-img.svg" alt="" class="login-say-img">
-            <img src="<?=$this->theme_base_url?>/assets/images/login-mail-img.svg" alt="" class="login-mail-img">
-            <img src="<?=$this->theme_base_url?>/assets/images/login-tree-img.svg" alt="" class="login-tree-img">
-            <img src="<?=$this->theme_base_url?>/assets/images/login-wall-bg.png" alt="" class="login-wall-bg">
-            <div class="login-img-part">
-                <img src="<?=$this->theme_base_url?>/assets/images/login-girl-img.svg" alt="">
+            <div class="col-xxl-12 xol-xl-12 xol-lg-12 text-center">
+                <h1>My Account</h1>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="./index.php">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">My Account</li>
+                    </ol>
+                </nav>
             </div>
-          </div>
         </div>
     </div>
-  </div>
+</section>
+
+<div class="login-section p-100">
+    <img src="./assets/images/login-imges/login-wave-img.png" alt="" class="login-wave-img">
+    <div class="container">
+        <div class="row">
+            <div class="col-xxl-6 col-lg-6 col-md-6">
+                <!-- --------login-left-content---- -->
+                <div class="login-left-content">
+                    <!-- ---------login-deails-wrappper----- -->
+                    <div class="login-deatils-wrapper">
+                        <h2 class="title">Welcome back</h2>
+                        <p class="pera">Welcome back! Enter your credentials to access your account.</p>
+                        <form action="">
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="mikehussy@gmail.com">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Phone</label>
+                                <input type="tel" class="form-control" id="exampleInputPassword1" placeholder="+1 412-450-3173">
+                            </div>
+                            <a href="./forgot-password-page.php" class="forgot-password-text">Forgot Password?</a>
+                            <div class="sign-in-btn tab-save-btn">
+                                <input type="submit" value="submit">
+                            </div>
+                            <p>Or, log in with your socials</p>
+
+                            <div class="log-in-btn">
+                                <a href="#" class="google-btn"><span><i class="fa-brands fa-google"></i></span>Login with Google</a>
+                                <a href="#" class="facebook-btn"><span><i class="fa-brands fa-facebook"></i></span>Login with Facebook</a>
+                            </div>
+
+                            <h3>Dont’ hvae an account? <a href="./sign-up-page.php">Sign up</a></h3>
+                        </form>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <!-- ------login-right-image-- -->
+            <div class="login-images col-xxl-6 col-lg-6 col-md-6">
+                <img src="./assets/images/login-imges/login-sub-img-1.png" alt="" class="login-sub-img-1">
+                <img src="./assets/images/login-imges/login-sub-img-2.png" alt="" class="login-sub-img-2">
+                <div class="login-img-part">
+                    <img src="./assets/images/login-imges/login-main-img.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
