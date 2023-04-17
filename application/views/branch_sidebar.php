240a903e75c3b3c8233d3996af860828c8bcc938
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-04-17 14:52:17 +0530

    Update branch_sidebar.php
    commit

--- application/views/branch_sidebar.php
+++ application/views/branch_sidebar.php
@@ -1,54 +1,48 @@
 <li class="sub-menu" style="display: 'none'">
-    <a href="javascript:;"
+    <!-- <a href="javascript:;"
         class="dcjq-parent <?php if ($this->uri->segment(1) == 'sell' || $this->uri->segment(1) == 'register' ||  $this->uri->segment(1) == "sell_development") { ?> active <?php } ?>">
         <i class="fa fa-tags"></i>
         <span>Sell</span>
-    </a>
+    </a> -->
     <ul class="sub">
         <!-- <li <?php echo ($this->uri->segment(2) == 'index') ? 'class="active"' : ''; ?> > <a href="<?php echo base_url() . 'sell/index/'; ?>">Sell</a></li> -->
-        <li <?php echo ($this->uri->segment(2) == 'index') ? 'class="active"' : ''; ?>> <a
-                href="<?php echo base_url() . 'sell_development/index/'; ?>">Sell development</a></li>
-        <li <?php echo ($this->uri->segment(2) == 'history') ? 'class="active"' : ''; ?>> <a
-                href="<?php echo base_url() . 'sell_development/history/'; ?>">Sell History</a></li>
-        <li <?php echo ($this->uri->segment(2) == 'parked_sell_list') ? 'class="active"' : ''; ?>> <a
-                href="<?php echo base_url() . 'sell/parked_sell_list/'; ?>">Parked Sell</a></li>
-        <li <?php echo ($this->uri->segment(2) == 'return_sell') ? 'class="active"' : ''; ?>> <a
-                href="<?php echo base_url() . 'sell_development/return_sell/'; ?>">Return Sell</a></li>
+        <li <?php echo ($this->uri->segment(2) == 'index') ? 'class="active"' : ''; ?>> <a href="<?php echo base_url() . 'sell_development/index/'; ?>">Sell development</a></li>
+        <li <?php echo ($this->uri->segment(2) == 'history') ? 'class="active"' : ''; ?>> <a href="<?php echo base_url() . 'sell_development/history/'; ?>">Sell History</a></li>
+        <li <?php echo ($this->uri->segment(2) == 'parked_sell_list') ? 'class="active"' : ''; ?>> <a href="<?php echo base_url() . 'sell/parked_sell_list/'; ?>">Parked Sell</a></li>
+        <li <?php echo ($this->uri->segment(2) == 'return_sell') ? 'class="active"' : ''; ?>> <a href="<?php echo base_url() . 'sell_development/return_sell/'; ?>">Return Sell</a></li>
         <!-- new end -->
         <?php if (!empty($register_result)) { ?>
-        <?php if ($register_result[0]->type == '1') { ?>
-        <li <?php if (
+            <?php if ($register_result[0]->type == '1') { ?>
+                <li <?php if (
                         $this->uri->segment(1) == 'register' && $this->uri->segment(2) == 'open_register'
                     ) { ?> class="active" <?php } ?>>
-            <a href="<?php echo base_url() . 'register/open_register/'; ?>"> Open/Close</a>
-        </li>
-        <?php } else { ?>
-        <li <?php if (
+                    <a href="<?php echo base_url() . 'register/open_register/'; ?>"> Open/Close</a>
+                </li>
+            <?php } else { ?>
+                <li <?php if (
                         $this->uri->segment(1) == 'register' && $this->uri->segment(2) == 'close_register'
                     ) { ?> class="active" <?php } ?>>
-            <a href="<?php echo base_url() . 'register/close_register/'; ?>"> Open/Close</a>
-        </li>
-        <?php } ?>
+                    <a href="<?php echo base_url() . 'register/close_register/'; ?>"> Open/Close</a>
+                </li>
+            <?php } ?>
         <?php } else { ?>
-        <li <?php if (
+            <li <?php if (
                     $this->uri->segment(1) == 'register' && $this->uri->segment(2) == 'close_register'
                 ) { ?> class="active" <?php } ?>>
-            <a href="<?php echo base_url() . 'register/close_register/'; ?>"> Open/Close</a>
-        </li>
+                <a href="<?php echo base_url() . 'register/close_register/'; ?>"> Open/Close</a>
+            </li>
         <?php } ?>
     </ul>
 </li>
 <li class="sub-menu" style="display: none;">
-    <a href="javascript:;"
-        class="dcjq-parent <?php if ($this->uri->segment(1) == 'stock_control' || $this->uri->segment(1) == 'supplier') { ?> active <?php } ?>">
+    <a href="javascript:;" class="dcjq-parent <?php if ($this->uri->segment(1) == 'stock_control' || $this->uri->segment(1) == 'supplier') { ?> active <?php } ?>">
         <i class="fa fa-tags"></i>
         <span>Product Management</span>
     </a>
     <ul class="sub">
         <li <?php if (
                 $this->uri->segment(1) == 'stock_control' || $this->uri->segment(2) == 'view_stock_control_list'
-            ) { ?> class="active" <?php } ?>><a
-                href="<?php echo base_url() . 'stock_control/view_stock_control_list'; ?>">Stock Control</a>
+            ) { ?> class="active" <?php } ?>><a href="<?php echo base_url() . 'stock_control/view_stock_control_list'; ?>">Stock Control</a>
         </li>
         <li <?php if (
                 $this->uri->segment(1) == 'supplier' || $this->uri->segment(2) == 'index'
@@ -57,8 +51,7 @@
     </ul>
 </li>
 <li class="sub-menu" style="display: none;">
-    <a href="javascript:;"
-        class="dcjq-parent <?php if ($this->uri->segment(1) == 'report' || $this->uri->segment(1) == 'sell_report') { ?> active <?php } ?>">
+    <a href="javascript:;" class="dcjq-parent <?php if ($this->uri->segment(1) == 'report' || $this->uri->segment(1) == 'sell_report') { ?> active <?php } ?>">
         <i class="fa fa-bar-chart-o"></i>
         <span>Report</span>
     </a>
@@ -79,8 +72,7 @@
         </li>
         <li <?php if (
                 $this->uri->segment(1) == 'report' && $this->uri->segment(2) == 'inventory_reports'
-            ) { ?> class="active" <?php } ?>><a
-                href="<?php echo base_url() . 'report/inventory_reports/'; ?>">Inventory Reports</a>
+            ) { ?> class="active" <?php } ?>><a href="<?php echo base_url() . 'report/inventory_reports/'; ?>">Inventory Reports</a>
         </li>
     </ul>
 </li>
@@ -124,15 +116,13 @@
     </a>
     <ul class="sub">
         <li>
-            <a class="<?php if ($this->uri->segment(1) == 'delivery' && $this->uri->segment(2) == 'delivery_list') { ?> active <?php } ?>"
-                href="<?php echo base_url() . 'delivery/delivery_list/'; ?>">
+            <a class="<?php if ($this->uri->segment(1) == 'delivery' && $this->uri->segment(2) == 'delivery_list') { ?> active <?php } ?>" href="<?php echo base_url() . 'delivery/delivery_list/'; ?>">
                 <i class="fa fa-users"></i>
                 <span>Delivery Staff</span>
             </a>
         </li>
         <li>
-            <a class="<?php if ($this->uri->segment(1) == 'staff') { ?> active <?php } ?>"
-                href="<?php echo base_url() . 'staff'; ?>">
+            <a class="<?php if ($this->uri->segment(1) == 'staff') { ?> active <?php } ?>" href="<?php echo base_url() . 'staff'; ?>">
                 <i class="fa fa-user"></i>
                 <span>Packaging Staff</span>
             </a>
@@ -141,53 +131,47 @@
 </li>
 
 <?php if ($this->countCategory == 1) { ?>
-<li>
-    <a class="<?php if ($this->uri->segment(1) == 'home_content') { ?> active <?php } ?>" href="#">
-        <i class="fa fa-home"></i>
-        <span>Home</span>
-    </a>
-    <ul>
-        <li>
-            <a class="<?php if ($this->uri->segment(1) == 'home_content') { ?> active <?php } ?>"
-                href="<?php echo base_url() . 'home_content'; ?>">
-                <i class="fa fa-user"></i>
-                <span>Home content</span>
-            </a>
-        </li>
-        <li>
-            <a class="<?php if ($this->uri->segment(2) == 'home_section_one' || $this->uri->segment(3) == 'add') { ?> active <?php } ?>"
-                href="<?php echo base_url() . 'home_content/home_section_one'; ?>">
-                <i class="fa fa-user"></i>
-                <span>Home section one</span>
-            </a>
-        </li>
-    </ul>
-</li>
+    <li>
+        <a class="<?php if ($this->uri->segment(1) == 'home_content') { ?> active <?php } ?>" href="#">
+            <i class="fa fa-home"></i>
+            <span>Home</span>
+        </a>
+        <ul>
+            <li>
+                <a class="<?php if ($this->uri->segment(1) == 'home_content') { ?> active <?php } ?>" href="<?php echo base_url() . 'home_content'; ?>">
+                    <i class="fa fa-user"></i>
+                    <span>Home content</span>
+                </a>
+            </li>
+            <li>
+                <a class="<?php if ($this->uri->segment(2) == 'home_section_one' || $this->uri->segment(3) == 'add') { ?> active <?php } ?>" href="<?php echo base_url() . 'home_content/home_section_one'; ?>">
+                    <i class="fa fa-user"></i>
+                    <span>Home section one</span>
+                </a>
+            </li>
+        </ul>
+    </li>
 <?php } ?>
 <li>
-    <a class="<?php if ($this->uri->segment(1) == 'category') { ?> active <?php } ?>"
-        href="<?php echo base_url() . 'category/category_list/'; ?>">
+    <a class="<?php if ($this->uri->segment(1) == 'category') { ?> active <?php } ?>" href="<?php echo base_url() . 'category/category_list/'; ?>">
         <i class="fa fa-list"></i>
         <span>Category</span>
     </a>
 </li>
 <li>
-    <a class="<?php if ($this->uri->segment(1) == 'subCategory') { ?> active <?php } ?>"
-        href="<?php echo base_url() . 'subCategory/'; ?>">
+    <a class="<?php if ($this->uri->segment(1) == 'subCategory') { ?> active <?php } ?>" href="<?php echo base_url() . 'subCategory/'; ?>">
         <i class="fa fa-list"></i>
         <span>Sub Category</span>
     </a>
 </li>
 <li>
-    <a class="<?php if ($this->uri->segment(1) == 'brand' && $this->uri->segment(2) == 'brand_list' || $this->uri->segment(2) == 'brand_profile') { ?> active <?php } ?>"
-        href="<?php echo base_url() . 'brand/brand_list/'; ?>">
+    <a class="<?php if ($this->uri->segment(1) == 'brand' && $this->uri->segment(2) == 'brand_list' || $this->uri->segment(2) == 'brand_profile') { ?> active <?php } ?>" href="<?php echo base_url() . 'brand/brand_list/'; ?>">
         <i class="fa fa-shield"></i>
         <span>Brand</span>
     </a>
 </li>
 <li>
-    <a class="<?php if ($this->uri->segment(1) == 'product' && $this->uri->segment(2) == 'product_list' || $this->uri->segment(2) == 'product_profile' || $this->uri->segment(2) == 'product_weight_list' || $this->uri->segment(2) == 'product_weight_profile' || $this->uri->segment(2) == 'product_image_list') { ?> active <?php } ?>"
-        href="<?php echo base_url() . 'product/product_list/'; ?>">
+    <a class="<?php if ($this->uri->segment(1) == 'product' && $this->uri->segment(2) == 'product_list' || $this->uri->segment(2) == 'product_profile' || $this->uri->segment(2) == 'product_weight_list' || $this->uri->segment(2) == 'product_weight_profile' || $this->uri->segment(2) == 'product_image_list') { ?> active <?php } ?>" href="<?php echo base_url() . 'product/product_list/'; ?>">
         <i class="fa fa-product-hunt"></i>
         <span>Product</span>
     </a>
@@ -211,53 +195,45 @@
     </ul>
 </li>
 <li>
-    <a class="<?php if ($this->uri->segment(1) == 'order' && $this->uri->segment(2) == '' && $this->uri->segment(2) != 'order_summary') { ?> active <?php } ?>"
-        href="<?php echo base_url() . 'order'; ?>">
+    <a class="<?php if ($this->uri->segment(1) == 'order' && $this->uri->segment(2) == '' && $this->uri->segment(2) != 'order_summary') { ?> active <?php } ?>" href="<?php echo base_url() . 'order'; ?>">
         <i class="fa fa-first-order"></i>
         <span>Order</span>
     </a>
 </li>
 <li>
-    <a class="<?php if ($this->uri->segment(1) == 'order' && $this->uri->segment(2) == 'order_report') { ?> active <?php } ?>"
-        href="<?php echo base_url() . 'order/order_report'; ?>">
+    <a class="<?php if ($this->uri->segment(1) == 'order' && $this->uri->segment(2) == 'order_report') { ?> active <?php } ?>" href="<?php echo base_url() . 'order/order_report'; ?>">
         <i class="fa fa-file"></i>
         <span>Product Wise Report</span>
     </a>
 </li>
 <li>
-    <a class="<?php if ($this->uri->segment(2) == 'order_summary') { ?> active <?php } ?>"
-        href="<?php echo base_url() . 'order/order_summary'; ?>">
+    <a class="<?php if ($this->uri->segment(2) == 'order_summary') { ?> active <?php } ?>" href="<?php echo base_url() . 'order/order_summary'; ?>">
         <i class="fa fa-file"></i>
         <span>Order summary</span>
     </a>
 </li>
 <li style="display: none;">
-    <a class="<?php if ($this->uri->segment(1) == 'subscription') { ?> active <?php } ?>"
-        href="<?php echo base_url() . 'subscription/vendor_detail'; ?>">
+    <a class="<?php if ($this->uri->segment(1) == 'subscription') { ?> active <?php } ?>" href="<?php echo base_url() . 'subscription/vendor_detail'; ?>">
         <i class="fa fa-envelope"></i>
         <span>Subcription</span>
     </a>
 </li>
 <li>
-    <a class="<?php if ($this->uri->segment(1) == 'payment_method') { ?> active <?php } ?>"
-        href="<?php echo base_url() . 'payment_method'; ?>">
+    <a class="<?php if ($this->uri->segment(1) == 'payment_method') { ?> active <?php } ?>" href="<?php echo base_url() . 'payment_method'; ?>">
         <i class="fa fa-credit-card" aria-hidden="true"></i>
         <span>Payment Method</span>
     </a>
 </li>
 <li style="display:none">
-    <a class="<?php if ($this->uri->segment(1) == 'banner_promotion' || $this->uri->segment(2) == 'web_banners') { ?> active <?php } ?>"
-        href="#">
+    <a class="<?php if ($this->uri->segment(1) == 'banner_promotion' || $this->uri->segment(2) == 'web_banners') { ?> active <?php } ?>" href="#">
         <i class="fa fa-picture-o"></i>
         <span>Banner</span>
-        <i
-            class="fas fa-chevron-right <?= ($this->uri->segment(1) == 'banner_promotion' || $this->uri->segment(2) == 'web_banners') ? 'rotate' : '' ?>"></i>
+        <i class="fas fa-chevron-right <?= ($this->uri->segment(1) == 'banner_promotion' || $this->uri->segment(2) == 'web_banners') ? 'rotate' : '' ?>"></i>
     </a>
     <ul>
 
         <li>
-            <a class="<?php if ($this->uri->segment(2) == 'web_banners' || $this->uri->segment(2) == 'add') { ?> active <?php } ?>"
-                href="<?php echo base_url() . 'admins/web_banners'; ?>">
+            <a class="<?php if ($this->uri->segment(2) == 'web_banners' || $this->uri->segment(2) == 'add') { ?> active <?php } ?>" href="<?php echo base_url() . 'admins/web_banners'; ?>">
                 <i class="fa fa-picture-o"></i>
                 <span>Web Banner </span>
             </a>
@@ -265,23 +241,20 @@
     </ul>
 </li>
 <li class="sub-menu" style="display:none">
-    <a href="javascript:;"
-        class="dcjq-parent <?php if ($this->uri->segment(1) == 'order' && ($this->uri->segment(2) == 'sell_report' || $this->uri->segment(2) == 'user_sell_report')) { ?> active <?php } ?>">
+    <a href="javascript:;" class="dcjq-parent <?php if ($this->uri->segment(1) == 'order' && ($this->uri->segment(2) == 'sell_report' || $this->uri->segment(2) == 'user_sell_report')) { ?> active <?php } ?>">
         <i class="fa fa-bar-chart-o"></i>
         <span>Sell Report</span>
         <i class="fas fa-chevron-right"></i>
     </a>
     <ul class="sub">
         <li class="<?= ($this->uri->segment(2) == 'sell_report') ? 'active' : '' ?>">
-            <a class="<?php if ($this->uri->segment(2) == 'sell_report') { ?> active <?php } ?>"
-                href="<?php echo base_url() . 'order/sell_report'; ?>">
+            <a class="<?php if ($this->uri->segment(2) == 'sell_report') { ?> active <?php } ?>" href="<?php echo base_url() . 'order/sell_report'; ?>">
                 <i class="fa fa-credit-card" aria-hidden="true"></i>
                 <span>Sell Report</span>
             </a>
         </li>
         <li class="<?= ($this->uri->segment(2) == 'user_sell_report') ? 'active' : '' ?>">
-            <a class="<?php if ($this->uri->segment(2) == 'user_sell_report') { ?> active <?php } ?>"
-                href="<?php echo base_url() . 'order/user_sell_report' ?>">
+            <a class="<?php if ($this->uri->segment(2) == 'user_sell_report') { ?> active <?php } ?>" href="<?php echo base_url() . 'order/user_sell_report' ?>">
                 <i class="fa fa-credit-card" aria-hidden="true"></i>
                 <span>User Sell Report</span>
             </a>
