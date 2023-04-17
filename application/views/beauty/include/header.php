b3d54095be56c2d98793e9fff0f19e6337a43ece
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-17 12:15:47 +0530

    Update header.php

--- application/views/beauty/include/header.php
+++ application/views/beauty/include/header.php
@@ -5,7 +5,7 @@
     
 
   <!-- stle-css -->
-  <link rel="stylesheet" href="<?=$theme_base_url()?>/assets/css/style.css">
+  <link rel="stylesheet" href="<?=$theme_base_url?>/assets/css/style.css">
 
   <!-- bootstrap-link -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -17,19 +17,19 @@
   <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"/>
   
   <!-- wow css -->
-  <link rel="stylesheet" href="<?=$theme_base_url()?>/assets/css/animate.css">
+  <link rel="stylesheet" href="<?=$theme_base_url?>/assets/css/animate.css">
 
       <!-- ------owl-min-css------ -->
-  <link rel="stylesheet" href="<?=$theme_base_url()?>/assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="<?=$theme_base_url?>/assets/css/owl.carousel.min.css">
 
   <!-- ------owl-theme-min-css------ -->
-  <link rel="stylesheet" href="<?=$theme_base_url()?>/assets/css/owl.theme.green.min.css">
+  <link rel="stylesheet" href="<?=$theme_base_url?>/assets/css/owl.theme.green.min.css">
 
   <!-- owl-carousel -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
 
   <!-- -----responsive-table-css--- -->
-  <link rel="stylesheet" href="<?=$theme_base_url()?>/assets/css/basictable.min.css">
+  <link rel="stylesheet" href="<?=$theme_base_url?>/assets/css/basictable.min.css">
   
   <!-- <link rel='stylesheet' href='https://unpkg.com/xzoom/dist/xzoom.css'> -->
 
