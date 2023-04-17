aaaf7b388958792586ee190937a2b887b4ef57b9
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-04-17 15:58:03 +0530

    Update checkout.js

--- public/frontend/assets/javascript/checkout.js
+++ public/frontend/assets/javascript/checkout.js
@@ -457,7 +457,7 @@ var CHECKOUT = (function () {
             orderAmount +
             (shipping_charge === "" ? 0 : parseFloat(shipping_charge)) -
             parseFloat(response.data)
-          ).toFixed(2);
+          ).toFixed(1);
           // console.log("orderAmount ====" ,orderAmount ,  parseFloat(shipping_charge) ,  parseFloat(response.data))
           if ($("#totalSaving").length) {
             var amount = response.data;
