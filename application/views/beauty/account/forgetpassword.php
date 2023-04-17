83038076305ccfeedece153b3f702d334170006d
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-17 19:52:30 +0530

    Update forgetpassword.php

--- application/views/beauty/account/forgetpassword.php
+++ application/views/beauty/account/forgetpassword.php
@@ -1,46 +1,73 @@
-
-  <!-- -----login-section----- -->
-  <div class="login-section forgot-section section">
+<!-- ----hero-section--- -->
+<section class="hero-section login-section common-banner-bg">
     <div class="container">
         <div class="row">
-          <div class="forgot-left col-xxl-6 col-lg-6 col-md-6">
-
-            <!-- --------login-left-content---- -->
-            <div class="login-left-content">
-              <h2 class="title">Forgot password?</h2>
-              <p class="pera">No worries, we'well send you reset intructions.</p>
-              
-              <!-- ---------forgot-deails-wrappper----- -->
-            <form id="ForgetForm" method="post" action="<?=base_url().'login/forget_password'?>">
-                <div class="login-deatils-wrapper signin-deatils-wrapper">
-                  <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email</label>
-                    <input type="email" name="email" class="form-control" placeholder="<?=$this->lang->line('Enter Email*')?>">
-                    <label for="email" class="error"><?=@form_error('email')?></label>
-                  </div>
-
-                  <div class="sign-in-btn reset-password-btn">
-                  <input class="common-input-btn" type="submit" id="btnSubmit"  value="<?=$this->lang->line('Reset Password')?>"/>
-                  <!-- <a href="#">Reset Password</a> -->
-                  </div> 
-
-                  <h3><span><i class="fa-solid fa-arrow-left"></i></span> Back to <a href="<?=base_url().'register'?>"><?=$this->lang->line('Sign up')?></a></h3>
-                </div>
-            </form>
-          </div>
-          </div>
-          
-          <!-- ------login-right-image-- -->
-          <div class="login-images col-xxl-6 col-lg-6 col-md-6">
-            <img src="<?=$this->theme_base_url?>/assets/images/forgot-stars-img.svg" alt="" class="login-say-img forgot-stars-img">
-            <!-- <img src="<?=$this->theme_base_url?>/assets/images/login-mail-img.svg" alt="" class="login-mail-img"> -->
-            <img src="<?=$this->theme_base_url?>/assets/images/forgot-plant-img.svg" alt="" class="forgot-plant-img">
-            <img src="<?=$this->theme_base_url?>/assets/images/login-wall-bg.png" alt="" class="login-wall-bg">
-
-            <div class="login-img-part forgot-password-img-part">
-                <img src="<?=$this->theme_base_url?>/assets/images/forgot-passowrd-main-img.svg" alt="">
+            <div class="col-xxl-12 xol-xl-12 xol-lg-12 text-center">
+                <h1><?=$this->lang->line('My Account')?></h1>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="<?=base_url().'home'?>"><?=$this->lang->line('home')?></a></li>
+                        <li class="breadcrumb-item active" aria-current="page"><?=$this->lang->line('My Account')?></li>
+                    </ol>
+                </nav>
             </div>
-          </div>
         </div>
     </div>
+</section>
+
+<div class="login-section forgot-section p-100">
+    <img src="./assets/images/login-imges/login-wave-img.png" alt="" class="login-wave-img">
+    <div class="container">
+        <div class="row">
+            <div class="col-xxl-6 col-lg-6 col-md-6">
+                <!-- --------login-left-content---- -->
+                <div class="login-left-content">
+
+                    <!-- ---------signin-deails-wrappper----- -->
+                    <div class="login-deatils-wrapper signin-deatils-wrapper">
+                    <h2 class="title">Forgot password?</h2>
+                        <p class="pera">No worries, we’well send you reset intructions.</p>
+                        <form action="">
+
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="mikehussy@gmail.com">
+                            </div>
+
+                            <div class="sign-in-btn reset-password-btn tab-save-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <button type="button">Reset Password</button>
+                            </div>
+
+                            <h3><span><i class="fa-solid fa-arrow-left"></i></span> Back to <a href="./sign-up-page.php">Sign up</a></h3>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <!-- ------login-right-image-- -->
+            <div class="login-images col-xxl-6 col-lg-6 col-md-6">
+                <img src="<?=$this->theme_base_url?>/assets/images/login-imges/login-sub-img-1.png" alt="" class="login-sub-img-1">
+                <img src="<?=$this->theme_base_url?>/assets/images/login-imges/login-sub-img-2.png" alt="" class="login-sub-img-2">
+                <div class="login-img-part">
+                    <img src="<?=$this->theme_base_url?>/assets/images/login-imges/login-main-img.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal common-modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-body">
+          <h3>Are you sure you want to change password?</h3>
+
+          <div class="reset-btn">
+              <button type="submit" class="yes-btn">Yes</button>
+              <button type="submit" class="no-btn">No</button>
+          </div>
+      </div>
+    </div>
   </div>
+</div>
