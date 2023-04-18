323a8b55bdd470922edb2db9bdd514f0733c075f
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-18 10:56:30 +0530

    Update home.js

--- public/beauty/assets/javascript/home.js
+++ public/beauty/assets/javascript/home.js
@@ -1,3 +1,49 @@
+$maxRange = 6;
+$multi = 0.1;
+$OldPosX = 0;
+$OldPosY = 0;
+$PositionY = -5;
+$PositionX = -5;
+$img = document.getElementById("Image");
+
+function moveMouse() {
+    CursorX = event.clientX;
+    CursorY = event.clientY;
+
+    if ($OldPosX <= CursorX) {
+        $PositionX = $PositionX + ($multi * 1);
+    } else {
+        $PositionX = $PositionX - ($multi * 1);
+    }
+
+    if ($PositionX >= $maxRange || $PositionX <= -$maxRange) {
+        if ($PositionX >= $maxRange) {
+            $PositionX = $maxRange;
+        } else {
+            $PositionX = -$maxRange;
+        }
+    }
+
+    $OldPosX = CursorX;
+
+    if ($OldPosY <= CursorY) {
+        $PositionY = $PositionY + ($multi * 1);
+    } else {
+        $PositionY = $PositionY - ($multi * 1);
+    }
+
+    if ($PositionY >= $maxRange || $PositionY <= -$maxRange) {
+        if ($PositionY >= $maxRange) {
+            $PositionY = $maxRange;
+        } else {
+            $PositionY = -$maxRange;
+        }
+    }
+
+
+    $OldPosY = CursorY;
+    $img.style.transform = "translateY(" + $PositionY + "%) translateX(" + $PositionX + "%)";
+}
 // $(document).on('click','.addcartbutton', function(){
 //     var that = $(this);
 //     var product_id = $(this).data('product_id');
