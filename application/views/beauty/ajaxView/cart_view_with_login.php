5bbb10dd16f4995becd329f735ed0af0df5a84f3
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-18 17:01:22 +0530

    Update cart_view_with_login.php

--- application/views/beauty/ajaxView/cart_view_with_login.php
+++ application/views/beauty/ajaxView/cart_view_with_login.php
@@ -29,7 +29,7 @@
             <a href="<?=base_url().'products/productDetails/'.$encode_id.'/'.$varient_id?>"> <?=$value->product_name?> </a>
         </h4>
           <p>Qty : <?=$value->quantity?></p>
-          <h3 class="notranslate"><?=$this->siteCurrency.' '.number_format((float)$product[0]->discount_price, 2, '.', '')?></h3>
+          <h3 class="notranslate"><?=$this->siteCurrency.' '.number_format((float)$value->discount_price, 2, '.', '')?></h3>
         </div>
         <div class="cancel-btn remove_item" data-product_id="<?=$value->product_id?>" data-product_weight_id="<?=$value->product_weight_id?>">
           <a href="javascript:"><i class="fa-regular fa-circle-xmark"></i></a>
