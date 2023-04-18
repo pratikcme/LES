165389b81f49e70dab4308291b3de0d2c7992e17
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-18 18:18:42 +0530

    Update addProduct.js

--- public/upbasket/assets/javascript/addProduct.js
+++ public/upbasket/assets/javascript/addProduct.js
@@ -297,9 +297,9 @@ var ADDPRODUCT = (function () {
 
           }
           var isDiscount = (output.discount_per > 0) ? "" : "d-none";
-          var disNwislist = '<span class="discnt ' + isDiscount + '">' + output.discount_per + "% off</span>";
+          var disNwislist = '<span class= discnt ' + isDiscount + '>' + output.discount_per + '% off</span>';
           disNwislist = disNwislist + '<div class="pro-hearticon wishlist-icon" data-product_id="' + output.product_id + '" data-product_weight_id="' + output.product_variant_id + '"><i class="fa-regular fa-heart ' + output.isInWishListUpbasket + '"></i></div>';
-          $(".discnt").remove();
+          // $(".discnt").remove();
           $(".pro-hearticon").remove();
           $("#zoom_image").before(disNwislist);
           // $(disNwislist).insertBefore('#zoom_image');
