d86f2f94d6b8699277a8ca8a0fda4edc9252ab49
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-19 12:03:39 +0530

    Update footer.php

--- application/views/beauty/include/footer.php
+++ application/views/beauty/include/footer.php
@@ -20,6 +20,7 @@ if (isset($_SESSION['user_id']) && $_SESSION['user_id'] != '') {
 
 <!-- jquery-cdn -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 <!-- bootstrap-datepicker-js -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
