cc6c73af5120fec908bf8fc62e3f034a58adbb65
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-20 19:34:51 +0530

    Update single_product.php

--- application/views/beauty/single_product.php
+++ application/views/beauty/single_product.php
@@ -169,16 +169,13 @@
                   <h3>Most Useful Review</h3>
 
                   <div class="supportive-div">
+                  <?php foreach ($product_review as $key => $value) { ?>
                     <div class="rewiew-wrapper">
                       <div class="review-left">
                         <div class="review-img">
                           <svg xmlns="./assets/images/review-icon.svg" viewBox="0 0 24.37 23.44">
                             <defs>
-                              <style>
-                                .cls-1 {
-                                  fill: #cc833d;
-                                }
-                              </style>
+                              <style>.cls-1 {fill: #cc833d; }</style>
                             </defs>
                             <path class="cls-1"
                               d="M25.06,23.78a.92.92,0,0,1-.34.34.88.88,0,0,1-.47.13H1.75a.88.88,0,0,1-.47-.13.92.92,0,0,1-.34-.34,1,1,0,0,1-.13-.47,1,1,0,0,1,.13-.47A13.94,13.94,0,0,1,8.68,16.5a8.43,8.43,0,0,1-.79-14A8.44,8.44,0,0,1,20.83,12.4a8.52,8.52,0,0,1-3.51,4.1,13.94,13.94,0,0,1,7.74,6.34,1,1,0,0,1,.13.47A1,1,0,0,1,25.06,23.78Z"
@@ -186,153 +183,22 @@
                           </svg>
                         </div>
                         <div class="review-text">
-                          <h6>Ashrita Jaiswal</h6>
+                          <h6><?=$value->fname.' '.$value->lname?></h6>
                           <p><span><i class="fa-solid fa-circle-check"></i></span> Verified Buyers</p>
                         </div>
                       </div>
                       <div class="review-right">
                         <div class="review-right-top">
                           <span class="number-star">5 <span><i class="fa-solid fa-star"></i></span></span>
-                          <h4>"I loved it"</h4>
+                          <!-- <h4>"I loved it"</h4> -->
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                          laboris nisi ut consequat. </p>
+                        <p><?=$value->review?></p>
                       </div>
                     </div>
-
-                    <div class="rewiew-wrapper">
-                      <div class="review-left">
-                        <div class="review-img">
-                          <svg xmlns="./assets/images/review-icon.svg" viewBox="0 0 24.37 23.44">
-                            <defs>
-                              <style>
-                                .cls-1 {
-                                  fill: #cc833d;
-                                }
-                              </style>
-                            </defs>
-                            <path class="cls-1"
-                              d="M25.06,23.78a.92.92,0,0,1-.34.34.88.88,0,0,1-.47.13H1.75a.88.88,0,0,1-.47-.13.92.92,0,0,1-.34-.34,1,1,0,0,1-.13-.47,1,1,0,0,1,.13-.47A13.94,13.94,0,0,1,8.68,16.5a8.43,8.43,0,0,1-.79-14A8.44,8.44,0,0,1,20.83,12.4a8.52,8.52,0,0,1-3.51,4.1,13.94,13.94,0,0,1,7.74,6.34,1,1,0,0,1,.13.47A1,1,0,0,1,25.06,23.78Z"
-                              transform="translate(-0.81 -0.81)" />
-                          </svg>
-                        </div>
-                        <div class="review-text">
-                          <h6>Ashrita Jaiswal</h6>
-                          <p><span><i class="fa-solid fa-circle-check"></i></span> Verified Buyers</p>
-                        </div>
-                      </div>
-                      <div class="review-right">
-                        <div class="review-right-top">
-                          <span class="number-star">5 <span><i class="fa-solid fa-star"></i></span></span>
-                          <h4>"I loved it"</h4>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                          laboris nisi ut consequat. </p>
-                      </div>
-                    </div>
-
-                    <div class="rewiew-wrapper">
-                      <div class="review-left">
-                        <div class="review-img">
-                          <svg xmlns="./assets/images/review-icon.svg" viewBox="0 0 24.37 23.44">
-                            <defs>
-                              <style>
-                                .cls-1 {
-                                  fill: #cc833d;
-                                }
-                              </style>
-                            </defs>
-                            <path class="cls-1"
-                              d="M25.06,23.78a.92.92,0,0,1-.34.34.88.88,0,0,1-.47.13H1.75a.88.88,0,0,1-.47-.13.92.92,0,0,1-.34-.34,1,1,0,0,1-.13-.47,1,1,0,0,1,.13-.47A13.94,13.94,0,0,1,8.68,16.5a8.43,8.43,0,0,1-.79-14A8.44,8.44,0,0,1,20.83,12.4a8.52,8.52,0,0,1-3.51,4.1,13.94,13.94,0,0,1,7.74,6.34,1,1,0,0,1,.13.47A1,1,0,0,1,25.06,23.78Z"
-                              transform="translate(-0.81 -0.81)" />
-                          </svg>
-                        </div>
-                        <div class="review-text">
-                          <h6>Ashrita Jaiswal</h6>
-                          <p><span><i class="fa-solid fa-circle-check"></i></span> Verified Buyers</p>
-                        </div>
-                      </div>
-                      <div class="review-right">
-                        <div class="review-right-top">
-                          <span class="number-star">5 <span><i class="fa-solid fa-star"></i></span></span>
-                          <h4>"I loved it"</h4>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                          laboris nisi ut consequat. </p>
-                      </div>
-                    </div>
-
-                    <div class="rewiew-wrapper">
-                      <div class="review-left">
-                        <div class="review-img">
-                          <svg xmlns="./assets/images/review-icon.svg" viewBox="0 0 24.37 23.44">
-                            <defs>
-                              <style>
-                                .cls-1 {
-                                  fill: #cc833d;
-                                }
-                              </style>
-                            </defs>
-                            <path class="cls-1"
-                              d="M25.06,23.78a.92.92,0,0,1-.34.34.88.88,0,0,1-.47.13H1.75a.88.88,0,0,1-.47-.13.92.92,0,0,1-.34-.34,1,1,0,0,1-.13-.47,1,1,0,0,1,.13-.47A13.94,13.94,0,0,1,8.68,16.5a8.43,8.43,0,0,1-.79-14A8.44,8.44,0,0,1,20.83,12.4a8.52,8.52,0,0,1-3.51,4.1,13.94,13.94,0,0,1,7.74,6.34,1,1,0,0,1,.13.47A1,1,0,0,1,25.06,23.78Z"
-                              transform="translate(-0.81 -0.81)" />
-                          </svg>
-                        </div>
-                        <div class="review-text">
-                          <h6>Ashrita Jaiswal</h6>
-                          <p><span><i class="fa-solid fa-circle-check"></i></span> Verified Buyers</p>
-                        </div>
-                      </div>
-                      <div class="review-right">
-                        <div class="review-right-top">
-                          <span class="number-star">5 <span><i class="fa-solid fa-star"></i></span></span>
-                          <h4>"I loved it"</h4>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                          laboris nisi ut consequat. </p>
-                      </div>
-                    </div>
-
-                    <div class="rewiew-wrapper">
-                      <div class="review-left">
-                        <div class="review-img">
-                          <svg xmlns="./assets/images/review-icon.svg" viewBox="0 0 24.37 23.44">
-                            <defs>
-                              <style>
-                                .cls-1 {
-                                  fill: #cc833d;
-                                }
-                              </style>
-                            </defs>
-                            <path class="cls-1"
-                              d="M25.06,23.78a.92.92,0,0,1-.34.34.88.88,0,0,1-.47.13H1.75a.88.88,0,0,1-.47-.13.92.92,0,0,1-.34-.34,1,1,0,0,1-.13-.47,1,1,0,0,1,.13-.47A13.94,13.94,0,0,1,8.68,16.5a8.43,8.43,0,0,1-.79-14A8.44,8.44,0,0,1,20.83,12.4a8.52,8.52,0,0,1-3.51,4.1,13.94,13.94,0,0,1,7.74,6.34,1,1,0,0,1,.13.47A1,1,0,0,1,25.06,23.78Z"
-                              transform="translate(-0.81 -0.81)" />
-                          </svg>
-                        </div>
-                        <div class="review-text">
-                          <h6>Ashrita Jaiswal</h6>
-                          <p><span><i class="fa-solid fa-circle-check"></i></span> Verified Buyers</p>
-                        </div>
-                      </div>
-                      <div class="review-right">
-                        <div class="review-right-top">
-                          <span class="number-star">5 <span><i class="fa-solid fa-star"></i></span></span>
-                          <h4>"I loved it"</h4>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                          laboris nisi ut consequat. </p>
-                      </div>
-                    </div>
-
                   </div>
-
+                  <?php } ?>
                 </div>
-                <div class="load-btn">
+                <div class="load-btn d-none">
                   <a href="#" class="cmn-btn lg-btn">Load More</a>
                 </div>
               </div>
