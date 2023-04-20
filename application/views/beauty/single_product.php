fac8808ab482344bbee784e7ba06820d21f21aba
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-20 18:48:29 +0530

    Update single_product.php

--- application/views/beauty/single_product.php
+++ application/views/beauty/single_product.php
@@ -56,16 +56,18 @@
           <div class=""></div>
           <h6 id="is_aval_stock"><?= ($varientDetails[0]->quantity > 25) ? $this->lang->line('Available(Instock)') : $this->lang->line('Limited Stock') ?></h6>
           <h2><?= $productDetail[0]->name ?></h2>
-          <h5 id="starRatting">
+          <div class="rating-starts">
+            <div class="rating stars3_5">
             <?php for ($j = 1; $j <= $productDetail[0]->rating['rating']; $j++) { ?>
-                <i class="fa-solid fa-star"></i>
+              <span class="star"></span>
             <?php } ?>
             <?php for ($i = 1; $i <= 5 - $productDetail[0]->rating['rating']; $i++) { ?>
-                <i class="fa-solid fa-star"></i> <!--  blank star appear hear -->
-                <!-- <i class="fas fa-star blank-ratting"></i> -->
+              <span class="star star-active"></span>
             <?php } ?>
-            <?= $productDetail[0]->rating['rating'] ?>  <span class="d-none"> <a href=""> 174 Ratings & 22 Reviews</a></span>
-          </h5>
+            </div>
+            <div><span>(<?= $productDetail[0]->rating['rating'] ?> )</span></div>
+          </div>
+        
 
           <h3 class="notranslate" id="dynamic_price">
             <?= $this->siteCurrency . ' ' . number_format((float)$varientDetails[0]->discount_price, 2, '.', '') ?>
