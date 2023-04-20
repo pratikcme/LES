a66492f72ea3923db9016746054cb2274f0e4e93
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-20 18:42:05 +0530

    Update single_product.php

--- application/views/beauty/single_product.php
+++ application/views/beauty/single_product.php
@@ -58,13 +58,10 @@
           <h2><?= $productDetail[0]->name ?></h2>
           <h5 id="starRatting">
             <?php for ($j = 1; $j <= $productDetail[0]->rating['rating']; $j++) { ?>
-                <!-- <i class="fa-star"></i> -->
-                <span class="star star-active"></span>
-                <?php } ?>
-                <?php for ($i = 1; $i <= 5 - $productDetail[0]->rating['rating']; $i++) { ?>
-                  <span class="star"></span>
-                <!-- <i class="fa-solid fa-star"></i>  -->
-                <!--  blank star appear hear -->
+                <i class="fa-solid fa-star"></i>
+            <?php } ?>
+            <?php for ($i = 1; $i <= 5 - $productDetail[0]->rating['rating']; $i++) { ?>
+                <i class="fa-solid fa-star"></i> <!--  blank star appear hear -->
                 <!-- <i class="fas fa-star blank-ratting"></i> -->
             <?php } ?>
             <?= $productDetail[0]->rating['rating'] ?>  <span class="d-none"> <a href=""> 174 Ratings & 22 Reviews</a></span>
