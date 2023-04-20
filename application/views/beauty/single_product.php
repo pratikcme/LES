fadbff179e4c5609c981ca4cfbbfae8a306c4633
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-20 16:41:59 +0530

    Update single_product.php

--- application/views/beauty/single_product.php
+++ application/views/beauty/single_product.php
@@ -77,13 +77,13 @@
           <h4><?=$this->lang->line('Categories')?>: <span><?= $productDetail[0]->category_name ?></span></h4>
           <!-- <h4>Tags: <span> Fresh, Oragnic</span></h4> -->
 
-          <!-- <form action="" class="pro-sl"> -->
-            <select class="form-control product_varient_id" id="cars">
+          <form action="" class="pro-sl">
+            <select class="product_varient_id" id="cars">
               <?php foreach ($varient as $key => $value) { ?>
                 <option value="<?= $this->utility->safe_b64encode($value) ?>" <?= ($varientDetails[0]->id == $value) ? 'selected' : '' ?>><?= $weight_no[$key] . ' ' . $weight_name[$key] ?></option>
               <?php } ?>
             </select>
-          <!-- </form> -->
+          </form>
 
           <!-- -----counter-product-- -->
           <div class="product-detail-quentity">
