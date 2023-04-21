708d7f59f508f2a97137e879781aa82e6253f48e
shahidCME <sahid.cmexpertise@gmail.com>
2023-04-21 12:58:34 +0530

    dfds

--- application/views/beauty/ajaxView/product_review_section.php
+++ application/views/beauty/ajaxView/product_review_section.php
@@ -26,9 +26,9 @@
             <div class="review-right">
                 <div class="review-right-top">
                     <span class="number-star"> <span><i class="fa-solid fa-star"></i></span><?=$value->ratting?></span>
-                    <h4><?=$value->review_title?></h4>
+                    <h4><?=$value->title?></h4>
                 </div>
-                <h6><?=$value->fname?> on <?=date('M d, Y')?> </h6>
+                <h6><?=$value->fname?> on <?=date('M d, Y',strtotime($value->dt_created))?></h6>
                 <p><?=$value->review?></p>
             </div>
         </div>
