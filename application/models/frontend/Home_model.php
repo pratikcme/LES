6b50e605ca08311d2d7d3d5fc84dbbe1d6f419d9
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-04-26 13:05:05 +0530

    Update Home_model.php

--- application/models/frontend/Home_model.php
+++ application/models/frontend/Home_model.php
@@ -327,7 +327,7 @@ class Home_model extends My_model
 			$data['table'] = 'my_cart';
 
 			$result_cart = $this->selectRecords($data);
-			lq();
+
 			if (count($result_cart) > 0) {
 				$my_cart_quantity = $result_cart[0]->quantity;
 			} else {
