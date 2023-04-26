a9e977ecf15eaae7df0447eaded9daaede4dc44a
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-04-26 13:09:14 +0530

    Update Home_model.php

--- application/models/frontend/Home_model.php
+++ application/models/frontend/Home_model.php
@@ -312,7 +312,7 @@ class Home_model extends My_model
 		$branch_id = $return[0]->branch_id;
 		foreach ($return as $k => $v) {
 			$product_variant_id = $v->product_varient_id;
-			$data['select'] = 'quantity';
+			$data['select'] = ['quantity'];
 			$data['where']['product_weight_id'] = $product_variant_id;
 			$data['where']['status !='] = 9;
 			$data['where']['branch_id'] = $branch_id;
@@ -326,7 +326,7 @@ class Home_model extends My_model
 			}
 			$data['table'] = 'my_cart';
 
-			$result_cart = $this->selectRecords($data);
+			$result_cart = @$this->selectRecords($data);
 
 			if (count($result_cart) > 0) {
 				$my_cart_quantity = $result_cart[0]->quantity;
