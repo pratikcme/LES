56e395908ec7b026ee309636b0a49cd1ac2ce6be
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-04-27 17:49:26 +0530

    Update dropdowns.js

--- public/shoppy/assets/js/dropdowns.js
+++ public/shoppy/assets/js/dropdowns.js
@@ -1,3 +1,14 @@
+// -----cart-sidenav-dropdown-js---
+
+let filterdiv = document.querySelector(".my-filter-wrapper");
+let leftdiv = document.querySelector(".show-div-wrapper");
+let hidedivebtn = document.querySelector(".filter-hide-btn");
+
+hidedivebtn.addEventListener("click", (event) => {
+  filterdiv.classList.toggle("hide");
+  leftdiv.classList.toggle("hide");
+});
+
 // ----search-icon-dropdown---
 // Open and Close Search Bar Toggle
 
@@ -25,11 +36,11 @@
 //    });
 
 //    searchBlock.addEventListener("focusin", () => {
-//     searchBlock.classList.add('is-active');  
+//     searchBlock.classList.add('is-active');
 //  });
-  
+
 //   mobilesearchblock.addEventListener("focusin", () => {
-//     mobilesearchblock.classList.add('is-active');  
+//     mobilesearchblock.classList.add('is-active');
 //  });
 
 //    document.addEventListener("focusout", () => {
@@ -39,7 +50,7 @@
 //  document.addEventListener("focusout", () => {
 //     mobilesearchblock.classList.remove('is-active');
 //  });
- 
+
 // }
 
 // Open and Close Search Bar Toggle
@@ -48,32 +59,24 @@ const searchToggle = document.querySelector(".search-toggle");
 const searchCancel = document.querySelector(".main-div-cancel");
 
 if (searchToggle && searchCancel) {
-   searchToggle.addEventListener("click", () => {
-      searchBlock.classList.add("is-active");
-      searchCancel.classList.add("is-active");
+  searchToggle.addEventListener("click", () => {
+    searchBlock.classList.add("is-active");
+    searchCancel.classList.add("is-active");
+  });
 
-   });
+  searchCancel.addEventListener("click", () => {
+    searchBlock.classList.remove("is-active");
+    searchCancel.classList.remove("is-active");
+  });
 
-   searchCancel.addEventListener("click", () => {
-      searchBlock.classList.remove("is-active");
-      searchCancel.classList.remove("is-active");
-   });
+  searchBlock.addEventListener("focusin", () => {
+    searchBlock.classList.add("is-active");
+  });
 
-   searchBlock.addEventListener("focusin", () => {
-    searchBlock.classList.add('is-active');  
- });
-
-   document.addEventListener("focusout", () => {
-    searchBlock.classList.remove('is-active');
- });
-
- 
+  document.addEventListener("focusout", () => {
+    searchBlock.classList.remove("is-active");
+  });
 }
-
-
-
-
-  
 
 // cart-pc-dropdwon-js--------
 
@@ -82,23 +85,20 @@ let carticons = document.querySelector(".cart-icons");
 let closebtns = document.querySelector(".close-btn");
 let cartdropdowns = document.querySelector(".cart-dropdwon");
 
-carticons.addEventListener('click', (event) => {
-  body.classList.toggle('show');
+carticons.addEventListener("click", (event) => {
+  body.classList.toggle("show");
 });
-carticons.addEventListener('click', (event) => {
-    cartdropdowns.classList.toggle('show');
+carticons.addEventListener("click", (event) => {
+  cartdropdowns.classList.toggle("show");
 });
-closebtns.addEventListener('click', (event) => {
-    cartdropdowns.classList.remove('show');
-    body.classList.remove('show');
+closebtns.addEventListener("click", (event) => {
+  cartdropdowns.classList.remove("show");
+  body.classList.remove("show");
 });
-body.addEventListener('click', (event) => {
-  cartdropdowns.classList.remove('show');
-  body.classList.remove('show');
+body.addEventListener("click", (event) => {
+  cartdropdowns.classList.remove("show");
+  body.classList.remove("show");
 });
-
-
-
 
 // ----user-dropdown-js---
 
@@ -113,8 +113,6 @@ body.addEventListener('click', (event) => {
 //   userdropdown.classList.remove('active');
 // });
 
-
-
 // // ---mobile-dropdown-cart-js---
 // -----------mobile-sidenav-------
 let mobileopenbtn = document.querySelector(".mobile-filter-btn");
@@ -122,39 +120,22 @@ let closebtn = document.querySelector(".closebtn");
 let mysidebar = document.querySelector(".sidepanel");
 let ovelaybg = document.querySelector(".mobile-btn-overlay");
 
-mobileopenbtn.addEventListener('click', (event) => {
-  mysidebar.classList.toggle('active');
-  ovelaybg.classList.toggle('active');
+mobileopenbtn.addEventListener("click", (event) => {
+  mysidebar.classList.toggle("active");
+  ovelaybg.classList.toggle("active");
 });
 
-closebtn.addEventListener('click', (event) => {
-  mysidebar.classList.remove('active');
-  ovelaybg.classList.remove('active');
+closebtn.addEventListener("click", (event) => {
+  mysidebar.classList.remove("active");
+  ovelaybg.classList.remove("active");
 });
 
-mobileopenbtn.addEventListener('blur', (event) => {
-  mysidebar.classList.remove('active');
-  ovelaybg.classList.remove('active');
+mobileopenbtn.addEventListener("blur", (event) => {
+  mysidebar.classList.remove("active");
+  ovelaybg.classList.remove("active");
 });
 
-ovelaybg.addEventListener('click', (event) => {
-  mysidebar.classList.remove('active');
-  ovelaybg.classList.remove('active');
+ovelaybg.addEventListener("click", (event) => {
+  mysidebar.classList.remove("active");
+  ovelaybg.classList.remove("active");
 });
-
-
-
-// -----cart-sidenav-dropdown-js---
-
-let filterdiv = document.querySelector(".my-filter-wrapper");
-let leftdiv = document.querySelector(".show-div-wrapper");
-let hidedivebtn = document.querySelector(".filter-hide-btn");
-
-hidedivebtn.addEventListener('click', (event) => {
-    filterdiv.classList.toggle('hide');
-    leftdiv.classList.toggle('hide');
-});
-
-
-
-
