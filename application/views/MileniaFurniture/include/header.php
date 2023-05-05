b39590933f2c7ec0a470ee19f2a0c2b81b1a11fc
shahidCME <101706296+shahidCME@users.noreply.github.com>
2023-05-05 16:00:03 +0530

    furniture design setup

--- application/views/MileniaFurniture/include/header.php
+++ application/views/MileniaFurniture/include/header.php
@@ -1,35 +1,49 @@
-<title><?= $this->siteTitle ?></title>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- meta tag -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- title -->
+    <title><?= $this->siteTitle ?></title>
+
+    <!-- css links -->
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- swiper -->
+    <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
+
+    <!-- drift -->
+    <link rel='stylesheet' href='https://s3-us-west-2.amazonaws.com/s.cdpn.io/164071/drift-basic.css' />
+
+   
+
+    <!-- owl.carousel.min -->
+    <link rel="stylesheet" href="<?=$this->theme_base_url.'/assets/css/owl.carousel.min.css'?>">
+
+    <!-- owl.theme -->
+    <link rel="stylesheet" href="<?=$this->theme_base_url.'/assets/css/owl.theme.default.min.css'?>">
+
+    <!-- -----responsive-table-css--- -->
+    <link rel="stylesheet" href="<?=$this->theme_base_url.'/assets/css/basictable.min.css'?>">
+
+    <!-- style -->
+    <link rel="stylesheet" href="<?=$this->theme_base_url.'/assets/css/style.css'?>">
+
+    <!-- animate -->
+    <link rel="stylesheet" href="<?=$this->theme_base_url.'/assets/css/animate.css'?>">
+
+    <!-- Bootstrap datepicker CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css"/>
+    <link rel='stylesheet' href='https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>
+<!-- <body dir="rtl" class="rtl"> -->
+<body>
 
 
-<!-- font-link -->
-<link rel="stylesheet" href="<?= $this->theme_base_url . '/assets/css/fonts.css' ?>">
-
-<!-- stle-css -->
-<link rel="stylesheet" href="<?= $this->theme_base_url . '/assets/css/style.css' ?>">
-<link rel="stylesheet" type="text/css" href="<?= base_url() ?>public/frontend/assets/css/bootstrap.min.css">
-<!-- bootstrap-link -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- font-awesome-cdn -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-
-<!-- wow css -->
-<link rel="stylesheet" href="<?= $this->theme_base_url . '/assets/css/animate.css' ?>">
-<link rel="stylesheet" href="<?= $this->theme_base_url . '/assets/css/slick.css' ?>">
-<!-- owl-carousel -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-<link rel='stylesheet' href='https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>
-<!-- swiper-css -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
-
-<!-- ----product-hover-zoom-css----- -->
-<link rel='stylesheet' href='https://s3-us-west-2.amazonaws.com/s.cdpn.io/164071/drift-basic.css'>
-
-<!-- Bootstrap datepicker CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" />
+<div class="overlay"></div>
 <?php $lang = json_encode($this->lang->language);
 // dd($lang);
 ?>
--- application/views/MileniaFurniture/include/mobile_navbar.php
+++ application/views/MileniaFurniture/include/mobile_navbar.php
@@ -1,13 +1,22 @@
-<!-- -----mobile-device-navbar--- -->
+
+<!-- -------mobile-device-navbar--- -->
 <div class="mobile-navbar">
-  <div class="container">
-    <div class="mobile-nav">
-      <ul>
-        <li><a href="<?= base_url() . 'home' ?>"><span><i class="fa fa-home" aria-hidden="true"></i></span>Home</a></li>
-        <li><a href="<?= base_url() . 'products' ?>"><span><i class="fa-solid fa-store"></i></span>Shop</a></li>
-        <li><a href="<?= base_url() . 'about' ?>"><span><i class="fa-solid fa-circle-info"></i></span>About</a></li>
-        <li><a href="<?= base_url() . 'contact' ?>"><span><i class="fa-regular fa-circle-user"></i></span>Contact</a></li>
-      </ul>
+    <div class="container">
+      <div class="mobile-nav">
+        <ul>
+          <li><a href="./index.php"><span><i class="fa fa-home" aria-hidden="true"></i></span>Home</a></li>
+          <li><a href="./p-listing-2.php"><span><i class="fa-solid fa-store"></i></span>Shop</a></li>
+          <li><a href="./sign-in-page.php"><span><i class="fa-regular fa-user"></i></span>Login</a></li>
+          <li><a href="./contact.php"><span><i class="fa-regular fa-circle-user"></i></span>Contact</a></li>
+        </ul>
+      </div>
     </div>
-  </div>
+</div>
+
+<div class="header-cart-in">
+    <a href="./shop-cart.php">
+      <div class=" header-cart-respo">
+        <i class="fa-solid fa-cart-shopping"></i>
+      </div>
+    </a>
 </div>
