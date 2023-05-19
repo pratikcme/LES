3a8fdccb8d2e4d08a619222cd298ebd0712f134c
shahidCME <101706296+shahidCME@users.noreply.github.com>
2023-05-19 16:14:37 +0530

    Update checkout.php

--- application/views/beauty/checkout.php
+++ application/views/beauty/checkout.php
@@ -611,6 +611,7 @@
                 <form id="OtpVerification" method="post" action="<?= base_url() . 'checkout/OtpVerification' ?>">
                     <label for="mobilenumber" class="form-label"><?= $this->lang->line('Please enter Otp') ?></label>
                     <input type="text" class="form-control" style="border:1px solid gray" name="otp" id="otp" placeholder="Please enter 4 digit otp*" maxlength="4" required="">
+                    <label id="invalid" style="display: none; color: red">Invalid Otp</label>
                     <br>
                     <button type="submit" id="btnSubmit"><?= $this->lang->line('Submit') ?></button>
                 </form>
