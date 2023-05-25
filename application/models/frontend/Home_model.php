88d8f006afb8fa96451b2b7501f46ec059b10a39
shahidCME <101706296+shahidCME@users.noreply.github.com>
2023-05-25 12:32:50 +0530

    Update Home_model.php

--- application/models/frontend/Home_model.php
+++ application/models/frontend/Home_model.php
@@ -275,13 +275,13 @@ class Home_model extends My_model
 		unset($data);
 		foreach ($result as $k => $v) {
 			if($_SERVER['REMOTE_ADDR']=='182.70.119.73'){
-				if ($v->end_date == $today && strtotime($v->end_time) >= strtotime($time)) {
+				if ($v->end_date == $today && $v->end_time <= $time) {
 					echo '1s';
 				}
 				
 				echo $v->end_time;
 				echo "<br>";
-				echo $time;
+				echo $today;
 			}
 			if ($v->end_date == $today && $v->end_time <= $time) {
 				unset($result[$k]);
