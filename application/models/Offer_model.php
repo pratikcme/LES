b70489e7b614086659cfcf1b62aafd3acf214ac5
shahidCME <101706296+shahidCME@users.noreply.github.com>
2023-05-25 13:39:52 +0530

    cron format

--- application/models/Offer_model.php
+++ application/models/Offer_model.php
@@ -87,7 +87,8 @@ Class Offer_model extends My_model{
         $st_hr = $st[0];
         $st_min = $st[1];
 
-        $utc_time =  gmdate("H:i",strtotime($st_array));
+        // $utc_time =  gmdate("H:i",strtotime($st_array));
+        $utc_time =  date("H:i", strtotime($st_array));
         $srvTime = date("H:i",strtotime($utc_time));
         $sts = explode(':',$srvTime);
         $st_hr = $sts[0];
@@ -173,7 +174,8 @@ Class Offer_model extends My_model{
         $end_hr = $st[0];
         $end_min = $st[1];
 
-        $utc_time =  gmdate("H:i",strtotime($end_array));
+        // $utc_time =  gmdate("H:i",strtotime($end_array));
+        $utc_time =  date("H:i", strtotime($end_array));
         $srvTime = date("H:i",strtotime($utc_time));
         $sts = explode(':',$srvTime);
         $st_hr = $sts[0];
@@ -262,7 +264,8 @@ Class Offer_model extends My_model{
             exec('crontab /var/www/html/stagging/crontab_final.txt 2>&1', $ext);
         }else{
 
-            $utc_time =  gmdate("H:i",strtotime($st_array));
+            // $utc_time =  gmdate("H:i",strtotime($st_array));
+            $utc_time =  date("H:i", strtotime($st_array));
             $srvTime = date("H:i",strtotime($utc_time));
             $sts = explode(':',$srvTime);
             // dd($sts);
