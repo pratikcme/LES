c770073f812dcf3ec0bed99b06550b16a69a17cc
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-06-16 15:14:54 +0530

    add dynamic css theme wise

--- application/views/beauty/include/header.php
+++ application/views/beauty/include/header.php
@@ -1,82 +1,86 @@
-<title><?=$this->siteTitle?></title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+<title><?= $this->siteTitle ?></title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link rel="stylesheet" type="text/css" href="<?=base_url()?>public/frontend/assets/css/bootstrap.min.css">
-  <!-- stle-css -->
-  <link rel="stylesheet" href="<?=$this->theme_base_url?>/assets/css/style.css">
 
-  <!-- bootstrap-link -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  
-  <!-- font-awesome-cdn -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+<link rel="stylesheet" type="text/css" href="<?= base_url() ?>public/frontend/assets/css/bootstrap.min.css">
+<!-- stle-css -->
+<?php $this->load->view('beauty/css/style.php'); ?>
+<link rel="stylesheet" href="<?= $this->theme_base_url ?>/assets/css/style.css">
 
-    <!-- -----swiper-slider-css----- -->
-  <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"/>
-  
-  <!-- wow css -->
-  <link rel="stylesheet" href="<?=$this->theme_base_url.'/assets/css/animate.css'?>">
+<!-- bootstrap-link -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-      <!-- ------owl-min-css------ -->
-  <link rel="stylesheet" href="<?=$this->theme_base_url.'/assets/css/owl.carousel.min.css'?>">
+<!-- font-awesome-cdn -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 
-  <!-- ------owl-theme-min-css------ -->
-  <link rel="stylesheet" href="<?=$this->theme_base_url.'/assets/css/owl.theme.green.min.css'?>">
+<!-- -----swiper-slider-css----- -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
 
-  <!-- owl-carousel -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+<!-- wow css -->
+<link rel="stylesheet" href="<?= $this->theme_base_url . '/assets/css/animate.css' ?>">
 
-  <!-- -----responsive-table-css--- -->
-  <link rel="stylesheet" href="<?=$this->theme_base_url.'/assets/css/basictable.min.css'?>">
-  
-  <!-- <link rel='stylesheet' href='https://unpkg.com/xzoom/dist/xzoom.css'> -->
+<!-- ------owl-min-css------ -->
+<link rel="stylesheet" href="<?= $this->theme_base_url . '/assets/css/owl.carousel.min.css' ?>">
 
-  <!-- ----product-hover-zoom-css----- -->
-  <link rel='stylesheet' href='https://s3-us-west-2.amazonaws.com/s.cdpn.io/164071/drift-basic.css'>
-  
-  <!-- Bootstrap datepicker CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css"/>
-  <link rel='stylesheet' href='https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>
-  <?php $lang = json_encode($this->lang->language);
-  // dd($lang);
-  ?>
+<!-- ------owl-theme-min-css------ -->
+<link rel="stylesheet" href="<?= $this->theme_base_url . '/assets/css/owl.theme.green.min.css' ?>">
+
+<!-- owl-carousel -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+
+<!-- -----responsive-table-css--- -->
+<link rel="stylesheet" href="<?= $this->theme_base_url . '/assets/css/basictable.min.css' ?>">
+
+<!-- <link rel='stylesheet' href='https://unpkg.com/xzoom/dist/xzoom.css'> -->
+
+<!-- ----product-hover-zoom-css----- -->
+<link rel='stylesheet' href='https://s3-us-west-2.amazonaws.com/s.cdpn.io/164071/drift-basic.css'>
+
+<!-- Bootstrap datepicker CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" />
+<link rel='stylesheet' href='https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>
+<?php $lang = json_encode($this->lang->language);
+// dd($lang);
+?>
 <script type="text/javascript">
-    var language = <?=$lang; ?>;
+  var language = <?= $lang; ?>;
 </script>
 <style>
+  /* google traslater*/
+  body {
+    top: 0px !important;
+  }
 
-/* google traslater*/
-body {
-top: 0px !important; 
-}
+  .goog-logo-link {
+    display: none !important;
+  }
 
-.goog-logo-link {
-    display:none !important;
-} 
-    
-.goog-te-gadget {
+  .goog-te-gadget {
     color: transparent !important;
-}
+  }
 
-.goog-te-banner-frame.skiptranslate {
-display: none !important;
-} 
-/* .goog-te-gadget .goog-te-combo {
+  .goog-te-banner-frame.skiptranslate {
+    display: none !important;
+  }
+
+  /* .goog-te-gadget .goog-te-combo {
     margin: 4px 0;
     width: 150px !important;
 } */
 
-.VIpgJd-ZVi9od-ORHb-OEVmcd.skiptranslate{
-  display:none !important;
-}
-.VIpgJd-ZVi9od-l4eHX-hSRGPd{
-  display :none !important;
-}
-.pac-container, .pac-item{
+  .VIpgJd-ZVi9od-ORHb-OEVmcd.skiptranslate {
+    display: none !important;
+  }
+
+  .VIpgJd-ZVi9od-l4eHX-hSRGPd {
+    display: none !important;
+  }
+
+  .pac-container,
+  .pac-item {
     z-index: 999999 !important;
- }
+  }
 </style>
-<input type="hidden" id="siteCurrency" value="<?=$this->siteCurrency?>">
+<input type="hidden" id="siteCurrency" value="<?= $this->siteCurrency ?>">
