a7ba43b0c1d11bfba4a77aaa186d32c053302828
Dipesh Khedkar <dipesh.cmexpertise@gmail.com>
2023-06-20 11:56:19 +0530

    Update Api_model.php

--- application/models/api_v3/Api_model.php
+++ application/models/api_v3/Api_model.php
@@ -1705,6 +1705,7 @@ class Api_model extends My_model
 
     public function checkShoppingBasedDiscount($cartTotal, $branch_id)
     {
+        dd(numberFormat($cartTotal));
         $query = $this->db->query('SELECT *,(' . $cartTotal . ' - cart_amount) AS CA FROM `amount_based_discount` where status = "1" AND branch_id = ' . $branch_id . ' HAVING CA > 0 ORDER BY CA ASC LIMIT 1');
         $re = $query->result();
         // lq();
