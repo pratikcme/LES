373b810ceacdc3bcb4aaedc988c128d62eb0351f
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-06-23 10:59:19 +0530

    Update footer.php

--- application/views/MileniaFurniture/include/footer.php
+++ application/views/MileniaFurniture/include/footer.php
@@ -291,7 +291,7 @@ if (!empty($js)) {
     function googleTranslateElementInit() {
         new google.translate.TranslateElement({
             pageLanguage: deLang,
-            includedLanguages: '<?=$supported_language?>'
+            includedLanguages: '<?= $supported_language ?>'
         }, 'google_translate_element');
     }
     // setTimeout(()=>{
@@ -324,7 +324,7 @@ if (!empty($js)) {
             }
             new google.translate.TranslateElement({
                 pageLanguage: deLang,
-                includedLanguages: '<?=$supported_language?>'
+                includedLanguages: '<?= $supported_language ?>'
             }, 'google_translate_element');
             // window.location.reload();        
             //     }
@@ -353,4 +353,10 @@ if (!empty($js)) {
     //     $(this).siblings().removeClass("active");
     //   });
     // });
+
+    $('.sub-menu ul').hide();
+    $(".sub-menu .right").click(function() {
+        $(this).parent().parent(".sub-menu").children("ul").slideToggle("100");
+        $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+    });
 </script>
