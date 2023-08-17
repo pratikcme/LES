5c0dd08ed227058565f03a8521890f0f4d29feda
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-08-17 11:40:47 +0530

    Update header.php

--- application/views/MileniaFurniture/include/header.php
+++ application/views/MileniaFurniture/include/header.php
@@ -88,14 +88,6 @@
                 z-index: 10000 !important;
             }
 
-            body {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                margin: 0;
-                background-color: #f4f4f4;
-            }
 
             .loader {
                 animation: spin 1s linear infinite;
