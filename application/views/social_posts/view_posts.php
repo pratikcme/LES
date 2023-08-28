2b1e27ad9e0eb09758fba908e5aa1a546fcef807
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-08-28 11:46:13 +0530

    Update view_posts.php

--- application/views/social_posts/view_posts.php
+++ application/views/social_posts/view_posts.php
@@ -106,48 +106,80 @@
         opacity: 0;
     }
 </style>
+
 <section id="main-content">
-    <div class="row">
-        <div class="col-lg-12 col-lg-12 mb-4">
-            <h5 class="title"><span>Independence day - </span><span>Tue 30 Aug 2023</span></h5>
-            <div class="row">
-                <div class="col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 mb-3">
-                    <div class="card-img month-card">
-                        <img src="<?= base_url() . 'public/festival_post_images/friendship_day.png' ?>" alt="india">
-                    </div>
-                </div>
+    <section class="wrapper">
+        <!-- page start-->
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <!--breadcrumbs start -->
+                <ul class="breadcrumb">
+                    <li class="active"><a href=""><i class="fa fa-home"></i> <a href="<?php echo base_url() . 'admin/index'; ?>">Home</a> / Social Media Post</a></li>
+                </ul>
+                <!--breadcrumbs end -->
             </div>
         </div>
-        <div class="col-lg-12 col-lg-12 mb-4">
-            <div class="d-flex input-wrapper flex-wrap">
-                <div class="d-flex flex-column input-box">
-                    <label for="">Quotes</label>
-                    <input type="text" placeholder="type here...........">
+        <div id="msg">
+            <?php if ($this->session->flashdata('msg') && $this->session->flashdata('msg') != '') { ?>
+                <div class="alert alert-success fade in">
+                    <strong>Success!</strong> <?php echo $this->session->flashdata('msg');; ?>
                 </div>
-                <div class="d-flex flex-column input-box">
-                    <label for="">Change Color</label>
-                    <input type="text" placeholder="#fff">
-                </div>
-                <div class="d-flex align-items-end">
-                    <button type="button">change</button>
-                </div>
-            </div>
+            <?php } ?>
         </div>
-        <div class="col-lg-12 col-lg-12 mb-4">
-            <h5 class="title">Preview</h5>
-            <div class="row">
-                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 mb-4">
-                    <div class="card-img position-relative preview-img">
-                        <img src="./assets/img/india.png" alt="">
-                        <div class="card-overlay"></div>
-                        <div class="download-icon">
-                            <img src="./assets/img/download.svg" alt="">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <section class="panel">
+                    <header class="panel-heading">Social Media Post</header>
+                    <div class="panel-body">
+
+                        <div class="row">
+                            <div class="col-lg-12 col-lg-12 mb-4">
+                                <h5 class="title"><span>Independence day - </span><span>Tue 30 Aug 2023</span></h5>
+                                <div class="row">
+                                    <div class="col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 mb-3">
+                                        <div class="card-img month-card">
+                                            <img src="<?= base_url() . 'public/festival_post_images/friendship_day.png' ?>" alt="india">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 col-lg-12 mb-4">
+                                <div class="d-flex input-wrapper flex-wrap">
+                                    <div class="d-flex flex-column input-box">
+                                        <label for="">Quotes</label>
+                                        <input type="text" placeholder="type here...........">
+                                    </div>
+                                    <div class="d-flex flex-column input-box">
+                                        <label for="">Change Color</label>
+                                        <input type="text" placeholder="#fff">
+                                    </div>
+                                    <div class="d-flex align-items-end">
+                                        <button type="button">change</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 col-lg-12 mb-4">
+                                <h5 class="title">Preview</h5>
+                                <div class="row">
+                                    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 mb-4">
+                                        <div class="card-img position-relative preview-img">
+                                            <img src="./assets/img/india.png" alt="">
+                                            <div class="card-overlay"></div>
+                                            <div class="download-icon">
+                                                <img src="./assets/img/download.svg" alt="">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+
                     </div>
-                </div>
+                </section>
             </div>
         </div>
-    </div>
+    </section>
 </section>
+
 
 <?php $this->load->view('footer.php'); ?>
