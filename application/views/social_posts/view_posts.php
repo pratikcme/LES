c04c4397aa4e5653b6e4d19b5377936ea87113f9
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-09-05 14:59:02 +0530

    Update view_posts.php

--- application/views/social_posts/view_posts.php
+++ application/views/social_posts/view_posts.php
@@ -3,7 +3,7 @@
 <div id="loader"></div>
 <style>
     .blurred {
-        filter: blur(2px);
+        filter: blur(5px);
         position: relative;
         z-index: 2;
         /* Adjust the blur intensity as needed */
