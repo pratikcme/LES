8137d1d7d518ec74c0e29bf373a4288326468025
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-09-11 18:58:30 +0530

    Update style.php

--- application/views/beauty/css/style.php
+++ application/views/beauty/css/style.php
@@ -1,6 +1,211 @@
 <?php
+echo $_SERVER['SERVER_NAME'];
+exit;
+if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == 'stagging.launchestore.com' || $_SERVER['SERVER_NAME'] == 'zhumkha.launchestore.com' ||  $_SERVER['SERVER_NAME'] == 'https://zhumkha.com') {
 
-if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == 'stagging.launchestore.com' || $_SERVER['SERVER_NAME'] == 'spawale.launchestore.com' ||  $_SERVER['SERVER_NAME'] == 'www.spawale.launchestore.com') {
+    $lightGreenBg = "rgba(234, 247, 237, 0.5)";
+    $lightWhiteBg = "#F5F5F5";
+    $commonPeraColor = "#FF0000";
+
+    $primaryColor = "#000000";
+    $secondaryColor = "#fed418";
+
+    $primaryPink = "#F92672";
+    $secondaryBlack = "#0F053F";
+    $lightBlack = "#868390";
+
+    $primaryBoxShadow = "0px 20px 50px rgba(43, 174, 74, 0.15)";
+
+    $titleFirstColor = "#000000";
+    $commonPeraColor = "#868390";
+    $commonBtnColor = "#11405A";
+    $commonBtnShadow = " 0px 7px 20px rgba(17, 64, 90, 0.2)";
+    $commonWhite = "#fff";
+    $btnHoverText = "#11405A";
+    $btnHoverShadow = "0px 3px 10px 1px rgba(17, 64, 90, 0.9)";
+    $selectBorderColor = "#C5CCD5";
+
+    $commonBorder = "#FEF8FA";
+    $commonBgColor = "linear-gradient(66.89deg, rgba(255, 248, 216, 0.5) 19.01%, rgba(255, 248, 216, 0.5) 100.66%);";
+
+    // -----breadcrumb-color--- 
+    $breadcrumbLinkColor = "#222528";
+    $breadcrumbActiveLinkColor = "#2BAE4A";
+
+    // / -----header-color---- /
+    $headerIconColorFirst = "#222528";
+    $headerIconColorSecond = "#FFFFFF";
+    $headerIconRoundColor = "#FFF5C6";
+    $headerTextWhite = "#FFFFFF";
+    $headerBtnBg = "#000000";
+    $headerBtnText = "#FFFFFF";
+
+
+    //  -----navigation-bar-color---- 
+    $navTextColor = "#000000";
+    $navHoverColor = "#FED418";
+
+    //  ------categorie-sec-color------
+    $categorieWrapperBorder = "#CCD3DB";
+    $categorieWrapperBg = "linear-gradient(66.89deg, #FFF8D8 19.01%, rgba(255, 248, 216, 0.28) 100.66%);";
+    $categorieWrapperBgHover = "#FED418";
+    $categorieWrapperRound = "#FED418";
+    $categorieWrapperText = "#000000";
+    $sliderBg = "#FFF5C6";
+
+    // -------card-color------
+    $cardBgColor = "#fff";
+    $cardBorderColor = "#FEF8FA";
+    $cardTextPrimary = "#000000";
+    $cardTextSecondary = "#555261";
+    $cardNumberPrimary = "#FED418";
+    $cardNumberSecondary = "#555261";
+    $cardBtnBg = "#000000";
+    $cardImgBg = "linear-gradient(66.89deg, #FFF8D8 100%, rgba(255, 248, 216, 0.28) 100.66%);";
+
+    // ----our-offer-color---
+    $offer1PrimaryText = "#F92672";
+    $offer1SecondaryText = "#0F053F";
+    $offer1Btn = "#0F053F";
+
+    $offer2PrimaryText = "#7749BE";
+    $offer2SecondaryText = "#7C0580";
+    $offer2Btn = "#7749BE";
+
+    // Three-banner-color
+    $cardTextColor = "#031B61";
+    $modernCardColor = "#A05500";
+    $practicalCardColor = "#035B3E";
+    $cardColor = "#3E8DC5";
+
+
+    // ----------footer-color------
+    $footerTextPrimary = "#000000";
+    $footerTextSecondary = "#555261";
+    $footerBgColor = "rgba(254,212,24,0.1)";
+    $footerLinkHover = "#FED418";
+    $copyRightBg = "#FED418";
+
+    /* ------------lisiting-colors--------- / */
+    $lisingBannerTitle1 = "#222528";
+    $lisingBannerTitle2 = "#2BAE4A";
+    $listingText1 = "#555261";
+    $filterIconColor1 = "#2BAE4A";
+    $listIconColor2 = "#222528";
+    $listingSubCtgBg = "#F2F5F3";
+    $listingSubCtgBorder = "#D9E2ED";
+    $listingSubCtgText = "#414549";
+    $listingSubCtgHover = "#2BAE4A";
+    $listingFilterIconBg = "#000000";
+
+    /* ------------lisiting-2-colors--------- */
+    $listing2CtgBg = "#FFFFFF";
+    $listing2CtgBorder = "linear-gradient(66.89deg, #FFF8D8 19.01%, rgba(255, 248, 216, 0.28) 100.66%);";
+    $listing2HeaderBg = "linear-gradient(66.89deg, #FFF8D8 19.01%, rgba(255, 248, 216, 0.28) 100.66%);";
+    $listing2HeaderText = "#000000";
+    $listing2NormalText = "#555261";
+
+    /* ------dropdown-cart------ / */
+    $dropCartText1 = "#0F053F";
+    $dropCartText2 = "#0F053F";
+    $dropCartNumber = "#FED418";
+    $dropCartBottomBorder = "#FED418";
+
+    /*  -----product-detalis-color----  */
+    $productDetailsText1 = "#000";
+    $productDetailsText2 = "#555261";
+    $productDetailsText3 = "#F92672";
+    $productDetailsText4 = "#FED418";
+    $productDetailsImageBg = "linear-gradient(66.89deg, rgba(255, 248, 216, 0.3) 19.01%, rgba(255, 248, 216, 0.3) 100.66%);";
+    $productDetailsBtn1 = "#0F053F";
+    $productDetailsBtn2 = "#2BAE4A";
+
+
+    /*  -------- shop-cart-color-------  */
+    $cartTheadBg = "linear-gradient(43.01deg, #FFF8DA 19.04%, rgba(255, 248, 218, 0.5) 69.77%)";
+    $cartTheadText = "#0F053F";
+    $cartTdText1 = "#0F053F";
+    $cartTdText2 = "#414549";
+    $cartTdText3 = "#FED418";
+    $cartImageBg = "linear-gradient(66.89deg, rgba(255, 248, 216, 0.5) 19.01%, rgba(255, 248, 216, 0.5) 100.66%);";
+    $cartCounterBorder = "#FFB9CE";
+    $cartCounterBg = "#fff";
+    $cartIcon = "#FED418";
+    $proceedCheckoutBtnBg = "#FED418";
+    $proceedCheckoutBtnText = "#fff";
+
+
+    /*  ----------------checkout-process-color-----  */
+    $accordianHeadBg = "linear-gradient(66.89deg, #FFF8D8 19.01%, rgba(255, 248, 216, 0.28) 100.66%);";
+    $accordianBg = "#fff";
+    $accordianBorder = "#FED418";
+    $accordianHeadText = "#000";
+    $accordianIcon = "#FFB9CE";
+    $accordianIconBg = "linear-gradient(66.89deg, #FFF8D8 19.01%, rgba(255, 248, 216, 0.28) 100.66%);";
+    $accordianContentText1 = "#000";
+    $accordianContentText2 = "#4D4C61";
+    $accordianStar = "#FFB9CE";
+    $checkoutSecureIcon = "linear-gradient(66.89deg, #FFF8D8 19.01%, rgba(255, 248, 216, 0.28) 100.66%);";
+
+
+    /*  -------modal-popup-color----  */
+    $modalBg = "rgba(17, 64, 90, 0.9)";
+    $modalText1 = "#F92672";
+    $modalText2 = "#0F053F";
+    $modalText3 = "#555261";
+    $continueBtnBg = "#F92672";
+    $continueBtnText = "#fff";
+    $closeBtn = "#F92672";
+
+
+    /*  -----------about-us-css--------  */
+    $peragraphColor = "#414549";
+    $clientWrapperText2 = "#222528";
+    $clientWrapperText3 = "#2BAE4A";
+    $clientWrapperBg = "#FFFFFF";
+    $clientWrapperBorder = "#D9E2ED";
+    $clientWrapperIcon = "#D9E2ED";
+
+
+    /*  ------myaccount-css---  */
+    $myaccoutnTabBg = "linear-gradient(66.89deg, #FFF8D8 19.01%, rgba(255, 248, 216, 0.28) 100.66%);";
+    $myaccoutnTabBorder = "#FED418";
+    $myaccoutnTabText = "#000";
+    $myaccoutnTabActiveBg = "#FED418";
+    $myaccoutnTabLabelText = "#0F053F";
+    $myaccoutnTabInputBg = "#FFFFFF";
+    $myaccoutnTabInputBorder = "#D0C9F1";
+
+
+    /*  login-page-color---  */
+    $loginTitleColor = "#000000";
+    $loginPeraColor = "#4E447D";
+    $loginWrapperBg = "#FFFFFF";
+    $loginWrapperBorder = "#FED418";
+    $loginWrapperLabel = "#000000";
+    $loginWrapperPlacholder = "#555261";
+    $loginWrapperInputBg = "#FFFFFF";
+    $loginWrapperInputBorder = "#D0C9F1";
+    $loginBtnBg = "#F92672";
+    $loginBtnText = "#FFF";
+    $forgotText = "#FED418";
+
+    /*  -----contact-us-css-------  */
+    $leftContactWrapperBg = "#FFFFFF";
+    $leftContactWrapperBorder = "#FED418";
+    $leftContactWrapperLabel = "#0F053F";
+    $leftContactWrapperInputBg = "#FFFFFF";
+    $leftContactWrapperInputBorder = "#D0C9F1";
+    $leftContactSubmitBtnBg = "#FED418";
+    $leftContactSubmitBtnText = "#FFF";
+
+    $rightContactWrapperBg = "#11405A";
+    $rightContactWrapperBorder = "#D9E2ED";
+    $rightContactIconRound = "rgba(254,212,24,0.2)";
+    $rightContactWrapperText = "#0F053F";
+
+    $new_svg_color = "#FFE9E4";
+} else if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == 'stagging.launchestore.com' || $_SERVER['SERVER_NAME'] == 'spawale.launchestore.com' ||  $_SERVER['SERVER_NAME'] == 'www.spawale.launchestore.com') {
 
     $lightGreenBg = "rgba(234, 247, 237, 0.5)";
     $lightWhiteBg = "#F5F5F5";
