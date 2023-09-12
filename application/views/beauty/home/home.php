ec9b2a845eaf4b3efcc86ff82cf80c3971b3b0b3
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-09-12 11:18:14 +0530

    Update home.php

--- application/views/beauty/home/home.php
+++ application/views/beauty/home/home.php
@@ -52,7 +52,9 @@
         <!-- --------owl-slider--------->
 
         <div class="owl-1 owl-carousel owl-theme">
-            <?php foreach ($category as $key => $value) { ?>
+            <?php
+            dd($category);
+            foreach ($category as $key => $value) { ?>
                 <a href="<?= base_url() . 'products?cat_id=' . $this->utility->safe_b64encode($value->id) ?>" class="categorie-wapper">
                     <div class="categorie-img">
                         <img src="<?= base_url() . 'public/images/' . $this->folder . 'category/' . $value->image ?>" alt="">
