dd67d0ba32c2c43e1b8c75c19c31419edd615254
cmerahul <rahul.cmexpertise@gmail.com>
2023-09-14 19:15:30 +0530

    commit

--- application/views/beauty/account/registration.php
+++ application/views/beauty/account/registration.php
@@ -21,83 +21,85 @@
                         </svg>
                         <h2 class="title">Sign Up</h2>
                         <p class="pera">Let’s create your account</p>
-                        <form action="">
+                        <form id="RegisterForm" method="post" action="<?= base_url() . 'register' ?>">
                             <div class="row">
-                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 mb-3">
                                     <div class="mb-3">
                                         <label for="fname" class="form-label">First Name</label>
-                                        <input type="text" class="form-control" id="fname" aria-describedby="fname" placeholder="enter firstname">
+                                        <input type="text" name="fname" class="form-control" id="fname" aria-describedby="fname" placeholder="<?= $this->lang->line('First Name*') ?>">
+                                        <!-- <label for="fname" class="error">Please enter first name</label> -->
                                     </div>
                                 </div>
 
-                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 mb-3">
                                     <div class="mb-3">
                                         <label for="lname" class="form-label">Last Name</label>
-                                        <input type="text" class="form-control" id="lname" aria-describedby="enter lirstname" placeholder="Hussy">
+                                        <input type="text" name="lname" class="form-control" id="lname" aria-describedby="lname" placeholder="<?= $this->lang->line('Last Name*') ?>">
+                                        <!-- <label for="lname" class="error">Please enter last name</label> -->
                                     </div>
                                 </div>
 
-                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 mb-3">
                                     <div class="mb-3 form-in">
                                         <label for="Country-code" class="form-label">Country Code<span>*</span></label>
-                                        <select class="form-select" aria-label="Country-code">
-                                            <option selected="">(+91) india </option>
-                                            <option value="1">Surt</option>
-                                            <option value="2">Baroda</option>
-                                            <option value="3">Ohter</option>
+                                        <select class="form-select" name="country_code" id="country_code" aria-label="Country-code">
+                                            <option value=""><?= $this->lang->line('select country code') ?></option>
+                                            <?php foreach (GetDialcodelist() as $key => $value) { ?>
+                                                <option value="<?= $key; ?>"><?= $value; ?></option>
+                                            <?php } ?>
                                         </select>
                                     </div>
                                 </div>
 
-                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 mb-3">
                                     <div class="mb-3">
-                                        <label for="Country-code" class="form-label">Phone</label>
-                                        <input type="tel" class="form-control" id="Country-code" aria-describedby="Country-code" placeholder="enter phone">
+                                        <label for="Country-code" class="form-label">Mobile Number</label>
+                                        <input type="tel" name="phone" class="form-control mob_no" aria-describedby="Country-code" placeholder="<?= $this->lang->line('Mobile Number*') ?>">
                                     </div>
                                 </div>
                                 
-                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 mb-3">
                                     <div class="mb-3">
                                         <label for="exampleInputEmail1" class="form-label">Email</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="enter email">
+                                        <input type="text" name="email" class="form-control" placeholder="<?= $this->lang->line('Email*') ?>">
                                     </div>
                                 </div>
                                 
-                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 mb-3">
                                     <div class="mb-3">
                                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="XXXXXXXXXX">
+                                        <input type="password" name="password" id="password" class="form-control" placeholder="<?= $this->lang->line('password*') ?>">
                                     </div>
                                 </div>
                                 
-                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 mb-3">
                                     <div class="mb-3">
                                         <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="XXXXXXXXXX">
+                                        <input type="password" class="form-control" name="confirm_password" id="confirm_password" placeholder="<?= $this->lang->line('Confirm password*') ?>">
                                     </div>
                                 </div>
 
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                        By creating an account, you agree to our <a href="#">Terms Of Conditions</a> and
-                                        <a href="#">Privacy Policy.</a>
+                                    <input class="form-check-input" type="checkbox" name="term_policy" id="flexCheckDefault">
+                                    <label class="form-check-label col-12" for="flexCheckDefault">
+                                        By creating an account, you agree to our <a href="<?= base_url() . 'terms_condition' ?>">Terms Of Conditions</a> and
+                                        <a href="<?= base_url() . 'privacy_policy' ?>"><?= $this->lang->line('Privacy Policy') ?>.</a>
                                     </label>
                                 </div>
+                                <label for="term_policy" class="error"></label>
 
                                 <div class="sign-in-btn tab-save-btn">
-                                    <input type="submit" value="submit">
+                                    <input type="submit" id="btnSubmit" value="<?= $this->lang->line("Sign up") ?>">
                                 </div>
 
-                                <p>Or, log in with your socials</p>
+                                <p><?= $this->lang->line('Or, log in with your socials') ?></p>
 
                                 <div class="log-in-btn">
-                                    <a href="#" class="google-btn"><span><i class="fa-brands fa-google"></i></span>Login
-                                        with Google</a>
-                                    <a href="#" class="facebook-btn"><span><i class="fa-brands fa-facebook"></i></span>Login with Facebook</a>
+                                    <a href="<?= $googleUrl ?>" class="google-btn"><span class="google-img"><img src="<?= base_url() . 'public/beauty/assets/images/' ?>google-btn.svg" alt="err"></span><?= $this->lang->line('continue with google') ?></a>
+                                    <a href="<?= base_url() . 'login/fb_login' ?>" class="facebook-btn"><span><i class="fa-brands fa-facebook"></i></span><?= $this->lang->line('continue with facebook') ?></a>
                                 </div>
 
-                                <h3>Already have an account? <a href="./sign-in-page.php">Sign in</a></h3>
+                                <h3>Already have an account? <a href="<?= base_url() . 'login' ?>"><?= $this->lang->line('Sign In') ?></a></h3>
 
                             </div>
                         </form>
