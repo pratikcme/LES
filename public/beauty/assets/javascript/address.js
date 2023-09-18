3c96f3837ccfe18919de6b540031c993b35fb020
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-09-18 14:57:38 +0530

    Update address.js

--- public/beauty/assets/javascript/address.js
+++ public/beauty/assets/javascript/address.js
@@ -329,7 +329,6 @@ var ADDRESS = (function () {
   });
 
   $(document).on("click", ".close", function () {
-    alert();
     $("#myModal").hide();
   });
   //  var  handleEditAddressForm =  function () {
