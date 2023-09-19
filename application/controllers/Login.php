7cdd9ee112f3f51828f4c470fbca1e87f8bf5e10
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-09-19 18:58:22 +0530

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -289,7 +289,8 @@ class Login extends User_Controller
 
 	public function user_register()
 	{
-
+		echo "hi";
+		exit;
 		$login_type = $this->this_model->checkLoginType();
 		if ($login_type == '0') {
 			redirect(base_url() . 'login');
