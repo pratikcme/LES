d6d62b39954fc172fb77a90c0f775922dff2c1ee
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-09-20 19:15:15 +0530

    commit

--- application/views/shoppy/account/registration.php
+++ application/views/shoppy/account/registration.php
@@ -16,23 +16,7 @@
     </div>
   </div>
 </section>
-<?php
 
-// Get the user's IP address
-$IPaddress = $_SERVER['REMOTE_ADDR'];
-
-
-$json       = file_get_contents("http://ipinfo.io/{$IPaddress}");
-$details    = json_decode($json);
-
-$country_phonecode = "";
-foreach (getCountryPhoneCode() as $key => $value) {
-  if ($key == $details->country) {
-    $country_phonecode = '+' . $value;
-  }
-}
-
-?>
 <div class="login-section p-100">
   <div class="container">
     <div class="row">
@@ -70,7 +54,7 @@ foreach (getCountryPhoneCode() as $key => $value) {
                   <select class="form-select" name="country_code" aria-label="Country-code" id="Country-code">
                     <option value=""><?= $this->lang->line('select country code') ?></option>
                     <?php foreach (GetDialcodelist() as $key => $value) { ?>
-                      <option <?= ($country_phonecode == $key) ? "selected" : "" ?> value="<?= $key; ?>"><?= $value; ?></option>
+                      <option <?= (getPhoneCode() == $key) ? "selected" : "" ?> value="<?= $key; ?>"><?= $value; ?></option>
                     <?php } ?>
                   </select>
                 </div>
