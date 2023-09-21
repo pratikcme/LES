56f0c3095f7b2ae42d888acc3f3655e4cc6d8e47
NidhiCME <nidhi.cmexpertise@gmail.com>
2023-09-21 13:28:08 +0530

    Update geolocation_helper.php

--- application/helpers/geolocation_helper.php
+++ application/helpers/geolocation_helper.php
@@ -8,12 +8,11 @@ use GeoIp2\Database\Reader;
 function lookupGeoIP()
 {
     // Use the MaxMind GeoIP2 library
-    echo APPPATH . 'config/maxmind-database.mmdb';
-    exit;
+
     $ipAddress = $_SERVER['REMOTE_ADDR'];
     // This reader object should be reused across lookups as creation of it is
     // expensive.
-    $reader = new Reader(FCPATH  . 'maxmind-database.mmdb');
+    $reader = new Reader(APPPATH . 'config/maxmind-database.mmdb');
 
 
     $record = $reader->city($ipAddress);
