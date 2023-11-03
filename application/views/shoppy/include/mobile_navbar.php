fc577027447bd02f0550a451ab529ba79fc93155
cmerahul <rahul.cmexpertise@gmail.com>
2023-11-03 18:14:34 +0530

    change in html

--- application/views/shoppy/include/mobile_navbar.php
+++ application/views/shoppy/include/mobile_navbar.php
@@ -5,7 +5,7 @@
       <ul>
         <li><a href="<?= base_url() . 'home' ?>"><span><i class="fa fa-home" aria-hidden="true"></i></span>Home</a></li>
         <li><a href="<?= base_url() . 'products' ?>"><span><i class="fa-solid fa-store"></i></span>Shop</a></li>
-        <li><a href="<?= base_url() . 'about' ?>"><span><i class="fa-solid fa-circle-info"></i></span>Login</a></li>
+        <li><a href="<?= base_url() . 'login' ?>"><span><i class="fa-solid fa-right-to-bracket"></i></span>Login</a></li>
         <li><a href="<?= base_url() . 'contact' ?>"><span><i class="fa-regular fa-circle-user"></i></span>Contact</a></li>
       </ul>
     </div>
