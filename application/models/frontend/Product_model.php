dcf2c52fd9a299570ba90f2b8b21ec806c7b6db5
cmexpertise <pm.cmexpertise@gmail.com>
2024-04-25 17:00:39 +0530

    Update Product_model.php

--- application/models/frontend/Product_model.php
+++ application/models/frontend/Product_model.php
@@ -1204,7 +1204,7 @@ class Product_model extends My_model
 		// $productId = $this->utility->safe_b64decode($product_id);
 
 		$data['table'] = TABLE_PRODUCT . " as p";
-		$data['select'] = ['p.name', 'pw.id as pw_id', 'pw.discount_per', 'pw.discount_price', 'pw.product_id as product_id', 'pi.image', 'pw.price', 'p.gst', 'pw.without_gst_price'];
+		$data['select'] = ['p.name', 'pw.id as pw_id', 'pw.id as weight_no', 'pw.discount_per', 'pw.discount_price', 'pw.product_id as product_id', 'pi.image', 'pw.price', 'p.gst', 'pw.without_gst_price'];
 		$data['join'] = [
 			TABLE_PRODUCT_WEIGHT . ' as pw' => ['p.id = pw.product_id', 'LEFT'],
 			TABLE_PRODUCT_IMAGE . ' as pi' => ['pw.id = pi.product_variant_id', 'LEFT']
