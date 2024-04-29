8eeecf7608978931ac9030292488857bf8f23509
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-29 14:46:00 +0530

    Update vendor.js

--- public/beauty/assets/javascript/vendor.js
+++ public/beauty/assets/javascript/vendor.js
@@ -139,7 +139,7 @@ $(document).ready(function () {
     document.body.clientHeight;
   $.ajax({
     type: "POST",
-    url: url + "home/setDimansion",
+    url: url + "vendors/setDimansion",
     data: {
       width: width,
       height: height,
