c5890c4ea9e752637d2071d65c9ae70569d0c5ae
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-01 17:13:56 +0530

    Update Sell_development_model.php

--- application/models/Sell_development_model.php
+++ application/models/Sell_development_model.php
@@ -186,7 +186,7 @@ class Sell_development_model extends My_model
 
         $this->load->model('api_v3/common_model', 'co_model');
         $isShow = $this->co_model->checkpPriceShowWithGstOrwithoutGst($this->session->userdata('branch_vendor_id'));
-        dd($isShow);
+        dd($isAvailable);
         if (!empty($isAvailable)) {
 
             if ($varient[0]->quantity >= $isAvailable[0]->quantity + $quantity) {
