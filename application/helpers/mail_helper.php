756d807c732f0f693478570c0f5ac14cc4fbe6cd
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-01 18:52:34 +0530

    Update mail_helper.php

--- application/helpers/mail_helper.php
+++ application/helpers/mail_helper.php
@@ -63,8 +63,9 @@ function getMycartSubtotal()
 
     $CI->load->model('frontend/product_model', 'product_model');
     $my_cart = $CI->product_model->getMyCart();
-    dd($my_cart);
+
     foreach ($my_cart as $key => $value) {
+      dd($value);
       if (!empty($isShow) && $isShow[0]->display_price_with_gst == '1') {
         $value->discount_price = $value->without_gst_price;
       }
