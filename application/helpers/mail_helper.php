e4c9c5dc433d12ffd3fdf077d09b863a39a4391b
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-01 18:53:11 +0530

    Update mail_helper.php

--- application/helpers/mail_helper.php
+++ application/helpers/mail_helper.php
@@ -69,6 +69,7 @@ function getMycartSubtotal()
       if (!empty($isShow) && $isShow[0]->display_price_with_gst == '1') {
         $value->discount_price = $value->without_gst_price;
       }
+      dd($value->discount_price);
       $total += numberFormat(numberFormat($value->discount_price) * $value->quantity);
     }
     // dd($total);
