d5b821d9c9498dbb6124bfdd6c53e928bbf57610
cmexpertise <pratik.cmexpertise@gmail.com>
2024-05-31 19:19:20 +0530

    Update style.php

--- application/views/shoppy/css/style.php
+++ application/views/shoppy/css/style.php
@@ -110,6 +110,131 @@ $new_svg_color = "#FFE9E4";
 
 if ($_SERVER['SERVER_NAME'] == 'kishoricreation.launchestore.com' ||  $_SERVER['SERVER_NAME'] == 'www.kishoricreation.launchestore.com') {
 
+// ==================== colors =============================
+$primaryColor = "#aa8434";
+$secondryColor = "#030235";
+$footerBg = "#000";
+$footerExtraBg = "rgba(196, 30, 73, 0.2)";
+$white = "#FFFFFF";
+
+// ========== heading-text-color ==========
+$headingBlack = '#030235';
+
+// =========== paregraph-text-color ==========
+$lightGray = "#4D4C61";
+$ofwhiteColor = "rgba(255, 255, 255, 0.7)";
+
+// =========== nav-hover-color ==============
+$navHoverColor = "#aa8434";
+
+// ============ banner-color ===========
+$firstBgColor = "#FFE9E4";
+$secondBgColor = "rgba(187 187 187 / 10%)";
+
+// ============== card-color ============
+$cardColor = "#3E8DC5";
+$cardTextColor = "#031B61";
+
+// ============== modern-card-color ==============
+$modernCardColor = "#A05500";
+
+// ============== practical-card-color ==============
+$practicalCardColor = "#035B3E";
+
+// ============= border-color =================
+$borderColor = "#C5CCD4";
+
+// =========== light-border-color ==========
+$lightBorderColor = "rgba(245, 81, 43, 0.4)";
+
+// ============== card-border ===============
+$cardBorder = "#E7DCDA";
+
+// ============= shop-cart-page-colors ============
+$cartTheadBg = "#FFF0ED";
+$cartTheadText = "#030235";
+$cartTdText1 = "#030235";
+$cartTdText2 = "#414549";
+$cartTdText3 = "#aa8434";
+$cartCounterBorder = "#FFCEC2";
+$cartCounterBg = "#fff";
+$cartIcon = "#aa8434";
+$proceedCheckoutBtnBg = "#aa8434";
+$proceedCheckoutBtnText = "#fff";
+
+// ================= checkout-process-color ===========
+$accordianhHeadBg = "#FFF0ED";
+$accordianBg = "#fff";
+$accordianBorder = "#FFCEC2";
+$accordianHeadText = "#0F053F";
+$accordianIcon = "#F92672";
+$accordianContentText1 = "#0F053F";
+$accordianContentText2 = "#555261";
+$accordianStar = "#FF1D1D";
+
+/* -------modal-popup-color---- */
+$modalBg =  "rgba(17, 64, 90, 0.9)";
+$modalText1 = "#294695";
+$modalText2 = "#030235";
+$modalText3 = "#4D4C61";
+$continueBtnBg = "#aa8434";
+$continuebtntext = "#fff";
+$closeBtn = "#aa8434";
+
+
+/* ------myaccount-css--- */
+$myaccoutnTabBg = "linear-gradient(43.01deg, #FFE9F1 19.04%, rgba(255, 233, 241, 0.28) 69.77%);;";
+$myaccoutntabborder = "#FFCEC2";
+$myaccoutntabtext = "#4D4C61";
+$myaccoutntabactivebg = "#aa8434";
+$myaccoutntabactiveborder = "#aa8434";
+$myaccoutntablabeltext = "#030235";
+$myaccoutntabinputbg = "#FFFFFF";
+$myaccoutntabinputborder = "#aa8434";
+
+$categoryheader = "#FFF0ED";
+
+/*---------- pagination ----------*/
+$pageborder = "#E7DCDA";
+$nextpage = "#FDDCD5";
+
+$commonborder = "rgba(196, 30, 73, 0.2)";
+$supportivecolor = "#FFF0ED";
+
+$bannerbgextra = "#aa8434";
+$aboutboxbg = "#aa8434";
+
+
+/* ----our-offer-color--- */
+$offer1primarytext = "#F92672";
+$offer1secondarytext = "#0F053F";
+$offer1btn = "#0F053F";
+
+$offer2primarytext = "#7749BE";
+$offer2secondarytext = "#7C0580";
+$offer2btn = "#7749BE";
+
+
+$new_svg_color = "#FFE9E4";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         $primaryColor = "#aa8434";
         $secondaryColor = "#1A1A1A";
         $lightColor =  "#fff";
