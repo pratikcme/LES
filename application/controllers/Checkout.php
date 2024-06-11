60a5254086259a62ababf7861b1c7ab22ea9515a
cmexpertise <pratik.cmexpertise@gmail.com>
2024-06-11 15:23:43 +0530

    Update Checkout.php

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -47,7 +47,7 @@ class Checkout extends User_Controller
 
     $data['isShow'] = $isShow;
     $data['checkout_line'] = $checkout_line;
-
+    dd($checkout_line);
 
     $this->load->model('api_v3/api_model');
     if (empty($_SESSION['My_cart']) && $this->cartCount == 0) {
