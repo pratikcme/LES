10176a3821c536ed22924d7ed7a13c7709ad9704
bhavisha-barot <132443595+bhavisha-barot@users.noreply.github.com>
2024-06-24 16:35:47 +0530

    Update body_header.php

--- application/views/shoppy/include/body_header.php
+++ application/views/shoppy/include/body_header.php
@@ -1,4 +1,28 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <section class="loader-main d-none">
+                    <!-- <div class="loader-wrapper">
+                        <div class="dots dot2">
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                            <div class="dot"></div>
+                        </div>
+                    </div> -->
+                </section>
+                <!-- -----header-top---- -->
+                <div class="header-top">
 
+                    <a href="<?= base_url() ?>">
+                        <div class="logo">
+                            <a href="<?= base_url() ?>"><img src=" <?= $this->siteLogo ?>" alt=""></a>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+</header>
 
 
 
