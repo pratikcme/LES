0117ee74ad5cdd26e024433ef9462730c2941fa4
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-06-24 11:05:04 +0530

    Update style.php

--- application/views/shoppy/css/style.php
+++ application/views/shoppy/css/style.php
@@ -107,156 +107,337 @@ $offer2btn = "#7749BE";
 
 
 $new_svg_color = "#FFE9E4";
+if ($_SERVER['SERVER_NAME'] == 'localhost'  || $_SERVER['SERVER_NAME'] == 'zumkha.launchestore.com' ||  $_SERVER['SERVER_NAME'] == 'zumkha.com' || $_SERVER['SERVER_NAME'] == 'www.zumkha.com') {
+    $lightGreenBg = "rgba(234, 247, 237, 0.5)";
+    $lightWhiteBg = "#F5F5F5";
+    $commonPeraColor = "#555261";
+    $smallBagColor = "#F6EAFD";
+    $primaryColor = "#005387";
+    $secondaryColor = "#1F1F1F";
 
+    $primaryPink = "#005387";
+    $secondaryBlack = "#45006B";
+    $lightBlack = "#868390";
+    $commonBannerBgColor = "#005387";
+    $primaryBoxShadow = "0px 20px 50px rgba(43, 174, 74, 0.15)";
+
+    $titleFirstColor = "#1F1F1F";
+    $commonPeraColor = "#868390";
+    $commonBtnColor = "#11405A";
+    $commonBtnShadow = "0px 7px 20px rgba(17, 64, 90, 0.2)";
+    $commonWhite = "#fff";
+    $btnHoverText = "#11405A";
+    $btnHoverShadow = "0px 3px 10px 1px rgba(17, 64, 90, 0.9)";
+    $selectBorderColor = "#C5CCD5";
+
+    $commonBorder = "#F6EAFD";
+
+    // -----breadcrumb-color--- 
+    $breadcrumbLinkColor = "#222528";
+    $breadcrumbActiveLinkColor = "#2BAE4A";
+
+    // / -----header-color---- /
+    $headerIconColorFirst = "#222528";
+    $headerIconColorSecond = "#FFFFFF";
+    $headerIconRoundColor = "rgba(0, 83, 135, 0.1)";
+    $headerTextWhite = "#FFFFFF";
+    $headerBtnBg = "#45006B";
+    $headerBtnText = "#FFFFFF";
+
+    //  -----navigation-bar-color---- 
+    $navTextColor = "#005387";
+    $navHoverColor = "#165e3c";
+
+    //  ------categorie-sec-color------
+    $categorieWrapperBorder = "#CCD3DB";
+    $categorieWrapperBg = "rgba(89, 65, 236, 0.1)";
+    $categorieWrapperBgHover = "#fc46ab";
+    $categorieWrapperRound = "linear-gradient(67deg, #F6EAFD 19.01%, rgba(246, 234, 253, 0.28) 100.66%)";
+    $categorieWrapperText = "#1F1F1F";
+
+    // -------card-color------
+    $cardBgColor = "#fff";
+    $cardBorderColor = "#F6EAFD";
+    $cardTextPrimary = "#0F053F";
+    $cardTextSecondary = "#555261";
+    $cardNumberPrimary = "#fe43ad";
+    $cardNumberSecondary = "#555261";
+    $cardBtnBg = "#005387";
+    $cardImgBg = "rgba(89, 65, 236, 0.1)";
+
+    // ----our-offer-color---
+    $offer1PrimaryText = "#F92672";
+    $offer1SecondaryText = "#45006B";
+    $offer1Btn = "#45006B";
+
+    $offer2PrimaryText = "#7749BE";
+    $offer2SecondaryText = "#7C0580";
+    $offer2Btn = "#7749BE";
+
+    // Three-banner-color
+    $cardTextColor = "#031B61";
+    $modernCardColor = "#A05500";
+    $practicalCardColor = "#035B3E";
+    $cardColor = "#3E8DC5";
+
+
+    // ----------footer-color------
+    $footerTextPrimary = "#0F053F";
+    $footerTextSecondary = "#555261";
+    $footerBgColor = "rgba(89, 65, 236, 0.1)";
+    $footerLinkHover = "#165e3c";
+    $copyRightBg = "#005387";
+
+    /* ------------lisiting-colors--------- / */
+    $lisingBannerTitle1 = "#222528";
+    $lisingBannerTitle2 = "#2BAE4A";
+    $listingText1 = "#555261";
+    $filterIconColor1 = "#2BAE4A";
+    $listIconColor2 = "#222528";
+    $listingSubCtgBg = "#F2F5F3";
+    $listingSubCtgBorder = "#D9E2ED";
+    $listingSubCtgText = "#414549";
+    $listingSubCtgHover = "#2BAE4A";
+    $listingFilterIconBg = "#165e3c";
+
+    /* ------------lisiting-2-colors--------- */
+    $listing2CtgBg = "#FFFFFF";
+    $listing2CtgBorder = "#F6EAFD";
+    $listing2HeaderBg = "rgba(89, 65, 236, 0.1)";
+    $listing2HeaderText = "#0F053F";
+    $listing2NormalText = "#555261";
+
+    /* ------dropdown-cart------ / */
+    $dropCartText1 = "#0F053F";
+    $dropCartText2 = "#45006B";
+    $dropCartNumber = "#165e3c";
+    $dropCartBottomBorder = "#FFEEF4";
+
+    /*  -----product-detalis-color----  */
+    $productDetailsText1 = "#0F053F";
+    $productDetailsText2 = "#555261";
+    $productDetailsText3 = "#165e3c";
+    $productDetailsText4 = "#165e3c";
+    $productDetailsImageBg = "rgba(89, 65, 236, 0.1)";
+    $productDetailsBtn1 = "#0F053F";
+    $productDetailsBtn2 = "#2BAE4A";
+
+
+    /*  -------- shop-cart-color-------  */
+    $cartTheadBg = "rgba(89, 65, 236, 0.1)";
+    $cartTheadText = "#0F053F";
+    $cartTdText1 = "#0F053F";
+    $cartTdText2 = "#414549";
+    $cartTdText3 = "#005387";
+    $cartImageBg = "rgba(89, 65, 236, 0.1)";
+    $cartCounterBorder = "#F6EAFD";
+    $cartCounterBg = "#fff";
+    $cartIcon = "#005387";
+    $proceedCheckoutBtnBg = "#1F1F1F";
+    $proceedCheckoutBtnText = "#fff";
+    $commonBgColor = "linear-gradient(90deg, rgba(132,115,237,0.16850490196078427) 0%, rgba(132,115,237,0.17130602240896353) 0%)";
+
+    /*  ----------------checkout-process-color-----  */
+    $accordianHeadBg = "rgba(89, 65, 236, 0.1)";
+    $accordianBg = "#fff";
+    $accordianBorder = "#F6EAFD";
+    $accordianHeadText = "#005387";
+    $accordianIcon = "#FFB9CE";
+    $accordianIconBg = "rgba(89, 65, 236, 0.1)";
+    $accordianContentText1 = "#030235";
+    $accordianContentText2 = "#4D4C61";
+    $accordianStar = "#FFB9CE";
+    $checkoutSecureIcon = "rgba(89, 65, 236, 0.1)";
+
+
+    /*  -------modal-popup-color----  */
+    $modalBg = "rgba(17, 64, 90, 0.9)";
+    $modalText1 = "#005387";
+    $modalText2 = "#0F053F";
+    $modalText3 = "#555261";
+    $continueBtnBg = "#005387";
+    $continueBtnText = "#fff";
+    $closeBtn = "#005387";
+
+
+    /*  -----------about-us-css--------  */
+    $peragraphColor = "#414549";
+    $clientWrapperText2 = "#222528";
+    $clientWrapperText3 = "#2BAE4A";
+    $clientWrapperBg = "#FFFFFF";
+    $clientWrapperBorder = "#D9E2ED";
+    $clientWrapperIcon = "#D9E2ED";
+
+
+    /*  ------myaccount-css---  */
+    $myaccoutnTabBg = "rgba(89, 65, 236, 0.1)";
+    $myaccoutnTabBorder = "#F6EAFD";
+    $myaccoutnTabText = "#0F053F";
+    $myaccoutnTabActiveBg = "#005387";
+    $myaccoutnTabLabelText = "#0F053F";
+    $myaccoutnTabInputBg = "#FFFFFF";
+    $myaccoutnTabInputBorder = "#D0C9F1";
+
+
+    /*  login-page-color---  */
+    $loginTitleColor = "#0F053F";
+    $loginPeraColor = "#4E447D";
+    $loginWrapperBg = "#FFFFFF";
+    $loginWrapperBorder = "#F6EAFD";
+    $loginWrapperLabel = "#0F053F";
+    $loginWrapperPlacholder = "#555261";
+    $loginWrapperInputBg = "#FFFFFF";
+    $loginWrapperInputBorder = "#D0C9F1";
+    $loginBtnBg = "#F92672";
+    $loginBtnText = "#FFF";
+    $forgotText = "#005387";
+
+    /*  -----contact-us-css-------  */
+    $leftContactWrapperBg = "#FFFFFF";
+    $leftContactWrapperBorder = "#F6EAFD";
+    $leftContactWrapperLabel = "#0F053F";
+    $leftContactWrapperInputBg = "#FFFFFF";
+    $leftContactWrapperInputBorder = "#D0C9F1";
+    $leftContactSubmitBtnBg = "#005387";
+    $leftContactSubmitBtnText = "#FFF";
+
+    $rightContactWrapperBg = "#11405A";
+    $rightContactWrapperBorder = "#D9E2ED";
+    $rightContactIconRound = "rgba(40, 104, 221, 0.1)";
+    $rightContactWrapperText = "#0F053F";
+    $new_svg_color = "rgba(89, 65, 236, 0.1)";
+}
 
 if ($_SERVER['SERVER_NAME'] == 'kishoricreation.launchestore.com' ||  $_SERVER['SERVER_NAME'] == 'www.kishoricreation.launchestore.com') {
 
-// ==================== colors =============================
-$primaryColor = "#aa8434";
-$secondryColor = "#030235";
-$footerBg = "#000";
-$footerExtraBg = "rgba(196, 30, 73, 0.2)";
-$white = "#FFFFFF";
+    // ==================== colors =============================
+    $primaryColor = "#aa8434";
+    $secondryColor = "#030235";
+    $footerBg = "#000";
+    $footerExtraBg = "rgba(196, 30, 73, 0.2)";
+    $white = "#FFFFFF";
 
-// ========== heading-text-color ==========
-$headingBlack = '#030235';
+    // ========== heading-text-color ==========
+    $headingBlack = '#030235';
 
-// =========== paregraph-text-color ==========
-$lightGray = "#4D4C61";
-$ofwhiteColor = "rgba(255, 255, 255, 0.7)";
+    // =========== paregraph-text-color ==========
+    $lightGray = "#4D4C61";
+    $ofwhiteColor = "rgba(255, 255, 255, 0.7)";
 
-// =========== nav-hover-color ==============
-$navHoverColor = "#aa8434";
+    // =========== nav-hover-color ==============
+    $navHoverColor = "#aa8434";
 
-// ============ banner-color ===========
-$firstBgColor = "#FFE9E4";
-$secondBgColor = "rgba(187 187 187 / 10%)";
+    // ============ banner-color ===========
+    $firstBgColor = "#FFE9E4";
+    $secondBgColor = "rgba(187 187 187 / 10%)";
 
-// ============== card-color ============
-$cardColor = "#3E8DC5";
-$cardTextColor = "#031B61";
+    // ============== card-color ============
+    $cardColor = "#3E8DC5";
+    $cardTextColor = "#031B61";
 
-// ============== modern-card-color ==============
-$modernCardColor = "#A05500";
+    // ============== modern-card-color ==============
+    $modernCardColor = "#A05500";
 
-// ============== practical-card-color ==============
-$practicalCardColor = "#035B3E";
+    // ============== practical-card-color ==============
+    $practicalCardColor = "#035B3E";
 
-// ============= border-color =================
-$borderColor = "#C5CCD4";
+    // ============= border-color =================
+    $borderColor = "#C5CCD4";
 
-// =========== light-border-color ==========
-$lightBorderColor = "rgba(245, 81, 43, 0.4)";
+    // =========== light-border-color ==========
+    $lightBorderColor = "rgba(245, 81, 43, 0.4)";
 
-// ============== card-border ===============
-$cardBorder = "#E7DCDA";
+    // ============== card-border ===============
+    $cardBorder = "#E7DCDA";
 
-// ============= shop-cart-page-colors ============
-$cartTheadBg = "#FFF0ED";
-$cartTheadText = "#030235";
-$cartTdText1 = "#030235";
-$cartTdText2 = "#414549";
-$cartTdText3 = "#aa8434";
-$cartCounterBorder = "#FFCEC2";
-$cartCounterBg = "#fff";
-$cartIcon = "#aa8434";
-$proceedCheckoutBtnBg = "#aa8434";
-$proceedCheckoutBtnText = "#fff";
+    // ============= shop-cart-page-colors ============
+    $cartTheadBg = "#FFF0ED";
+    $cartTheadText = "#030235";
+    $cartTdText1 = "#030235";
+    $cartTdText2 = "#414549";
+    $cartTdText3 = "#aa8434";
+    $cartCounterBorder = "#FFCEC2";
+    $cartCounterBg = "#fff";
+    $cartIcon = "#aa8434";
+    $proceedCheckoutBtnBg = "#aa8434";
+    $proceedCheckoutBtnText = "#fff";
 
-// ================= checkout-process-color ===========
-$accordianhHeadBg = "#FFF0ED";
-$accordianBg = "#fff";
-$accordianBorder = "#FFCEC2";
-$accordianHeadText = "#0F053F";
-$accordianIcon = "#F92672";
-$accordianContentText1 = "#0F053F";
-$accordianContentText2 = "#555261";
-$accordianStar = "#FF1D1D";
+    // ================= checkout-process-color ===========
+    $accordianhHeadBg = "#FFF0ED";
+    $accordianBg = "#fff";
+    $accordianBorder = "#FFCEC2";
+    $accordianHeadText = "#0F053F";
+    $accordianIcon = "#F92672";
+    $accordianContentText1 = "#0F053F";
+    $accordianContentText2 = "#555261";
+    $accordianStar = "#FF1D1D";
 
-/* -------modal-popup-color---- */
-$modalBg =  "rgba(17, 64, 90, 0.9)";
-$modalText1 = "#294695";
-$modalText2 = "#030235";
-$modalText3 = "#4D4C61";
-$continueBtnBg = "#aa8434";
-$continuebtntext = "#fff";
-$closeBtn = "#aa8434";
-
-
-/* ------myaccount-css--- */
-$myaccoutnTabBg = "linear-gradient(43.01deg, #FFE9F1 19.04%, rgba(255, 233, 241, 0.28) 69.77%);;";
-$myaccoutntabborder = "#FFCEC2";
-$myaccoutntabtext = "#4D4C61";
-$myaccoutntabactivebg = "#aa8434";
-$myaccoutntabactiveborder = "#aa8434";
-$myaccoutntablabeltext = "#030235";
-$myaccoutntabinputbg = "#FFFFFF";
-$myaccoutntabinputborder = "#aa8434";
-
-$categoryheader = "#FFF0ED";
-
-/*---------- pagination ----------*/
-$pageborder = "#E7DCDA";
-$nextpage = "#FDDCD5";
-
-$commonborder = "rgba(196, 30, 73, 0.2)";
-$supportivecolor = "#FFF0ED";
-
-$bannerbgextra = "#aa8434";
-$aboutboxbg = "#aa8434";
+    /* -------modal-popup-color---- */
+    $modalBg =  "rgba(17, 64, 90, 0.9)";
+    $modalText1 = "#294695";
+    $modalText2 = "#030235";
+    $modalText3 = "#4D4C61";
+    $continueBtnBg = "#aa8434";
+    $continuebtntext = "#fff";
+    $closeBtn = "#aa8434";
 
 
-/* ----our-offer-color--- */
-$offer1primarytext = "#F92672";
-$offer1secondarytext = "#0F053F";
-$offer1btn = "#0F053F";
+    /* ------myaccount-css--- */
+    $myaccoutnTabBg = "linear-gradient(43.01deg, #FFE9F1 19.04%, rgba(255, 233, 241, 0.28) 69.77%);;";
+    $myaccoutntabborder = "#FFCEC2";
+    $myaccoutntabtext = "#4D4C61";
+    $myaccoutntabactivebg = "#aa8434";
+    $myaccoutntabactiveborder = "#aa8434";
+    $myaccoutntablabeltext = "#030235";
+    $myaccoutntabinputbg = "#FFFFFF";
+    $myaccoutntabinputborder = "#aa8434";
 
-$offer2primarytext = "#7749BE";
-$offer2secondarytext = "#7C0580";
-$offer2btn = "#7749BE";
+    $categoryheader = "#FFF0ED";
 
+    /*---------- pagination ----------*/
+    $pageborder = "#E7DCDA";
+    $nextpage = "#FDDCD5";
 
-$new_svg_color = "#FFE9E4";
+    $commonborder = "rgba(196, 30, 73, 0.2)";
+    $supportivecolor = "#FFF0ED";
 
-
-
-
-
-
-
-
-
-
-
-
+    $bannerbgextra = "#aa8434";
+    $aboutboxbg = "#aa8434";
 
 
+    /* ----our-offer-color--- */
+    $offer1primarytext = "#F92672";
+    $offer1secondarytext = "#0F053F";
+    $offer1btn = "#0F053F";
+
+    $offer2primarytext = "#7749BE";
+    $offer2secondarytext = "#7C0580";
+    $offer2btn = "#7749BE";
 
 
+    $new_svg_color = "#FFE9E4";
 
-
-
-        $primaryColor = "#aa8434";
-        $secondaryColor = "#1A1A1A";
-        $lightColor =  "#fff";
-        $borderColor = "#aa8434";
-        $borderColor2 = "#aa8434";
-        $lightPrimary = "#ff774b";
-        $headerBackground = "#000";
-        $navbarColor = "#fff";
-        $cartBtnBackground = "#aa8434";
-        $cartBtnColor = "#fff";
-        $loginBtnBackground = "#aa8434";
-        $loginBtnColor = "#fff";
-        $loginBtnborder = "#aa8434";
-        $logoWidth = "200px";
-        $abtImgWidth = "350px";
-        $hvrClr = "#1A1A1A";
-        $cartNum =  "#000";
-        $categoryColor = "#000";
-        $categoryActiveBg = "#fff";
-        $categoryActiveColor = "#fff";
-    } 
+    $primaryColor = "#aa8434";
+    $secondaryColor = "#1A1A1A";
+    $lightColor =  "#fff";
+    $borderColor = "#aa8434";
+    $borderColor2 = "#aa8434";
+    $lightPrimary = "#ff774b";
+    $headerBackground = "#000";
+    $navbarColor = "#fff";
+    $cartBtnBackground = "#aa8434";
+    $cartBtnColor = "#fff";
+    $loginBtnBackground = "#aa8434";
+    $loginBtnColor = "#fff";
+    $loginBtnborder = "#aa8434";
+    $logoWidth = "200px";
+    $abtImgWidth = "350px";
+    $hvrClr = "#1A1A1A";
+    $cartNum =  "#000";
+    $categoryColor = "#000";
+    $categoryActiveBg = "#fff";
+    $categoryActiveColor = "#fff";
+}
 ?>
 
 
@@ -285,11 +466,11 @@ $new_svg_color = "#FFE9E4";
 
         /*========== heading-text-color ==========*/
         --heading-black: <?= $headingBlack ?>;
-        --headerBackground : <?= $headerBackground ?>;
-        --navbarColor : <?= $navbarColor ?>;
-        --cartBtnBackground : <?= $cartBtnBackground ?>;
-        --logoWidth :<?= $logoWidth?>;
-        --vendorDrop:<?= $vendorDrop?>;
+        --headerBackground: <?= $headerBackground ?>;
+        --navbarColor: <?= $navbarColor ?>;
+        --cartBtnBackground: <?= $cartBtnBackground ?>;
+        --logoWidth: <?= $logoWidth ?>;
+        --vendorDrop: <?= $vendorDrop ?>;
 
         /*=========== paregraph-text-color ==========*/
         --light-gray: <?= $lightGray ?>;
