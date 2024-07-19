9f44127de7e5a27d398e2a08185152696b34387e
cmexpertise <pratik.cmexpertise@gmail.com>
2024-07-19 18:07:42 +0530

    Update single_product.php

--- application/views/beauty/single_product.php
+++ application/views/beauty/single_product.php
@@ -81,17 +81,18 @@
 
 
                     <div class="pro-sl">
-                        <!-- <select class="product_varient_id" id="cars">
+                        <select class="product_varient_id" id="cars">
                             <?php foreach ($varient as $key => $value) { ?>
                                 <option value="<?= $this->utility->safe_b64encode($value) ?>" <?= ($varientDetails[0]->id == $value) ? 'selected' : '' ?>>
                                     <?= $weight_no[$key] . ' ' . $weight_name[$key] ?></option>
                             <?php } ?>
-                        </select> -->
-                        <!-- <?php foreach ($varient as $key => $value) { ?>
+                        </select>
+
+                        <?php foreach ($varient as $key => $value) { ?>
                             <img src="<?= base_url() . 'public/images/' . $this->folder . 'product_image/' . $product_image[$key] ?>" alt="">
                             <button class="variants-list" value="<?= $this->utility->safe_b64encode($value) ?>" <?= ($varientDetails[0]->id == $value) ? 'activated' : '' ?>> <?= $weight_no[$key] . ' ' . $weight_name[$key] ?> </button>
 
-                        <?php } ?> -->
+                        <?php } ?>
                     </div>
                     <?php
                     $d_none = '';
