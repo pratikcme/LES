ad4b64835422656bc6fe208b21762071bca4ec21
cmexpertise <pratik.cmexpertise@gmail.com>
2024-07-25 12:44:00 +0530

    Update home.php

--- application/views/beauty/home/home.php
+++ application/views/beauty/home/home.php
@@ -274,9 +274,9 @@ $responsive_height = $_SESSION['responsive_height'];
             </div>
         </div>
 
-        <div class="owl-theme wow fadeInDown" data-wow-duration="1s" data-wow-delay="0" data-wow-offset="0">
+        <div class="row owl-theme wow fadeInDown" data-wow-duration="1s" data-wow-delay="0" data-wow-offset="0">
             <?php foreach ($new_arrival as $key => $value) { ?>
-                <div class="techno-check">
+                <div class="techno-check col-md-3">
                     <input class="techno_checkbox" type="checkbox" id="1" value="1" />
                     <div href="#" class="product-wrapper card <?= ($value->varientQuantity == '0') ? 'out-of-stock' : '' ?>">
 
