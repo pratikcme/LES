a64ba4e171006d15dd64a17fae0802dfea707354
cmexpertise <pratik.cmexpertise@gmail.com>
2024-08-29 15:11:44 +0530

    Update Import.php

--- application/controllers/Import.php
+++ application/controllers/Import.php
@@ -403,13 +403,11 @@ class Import extends Vendor_Controller
                 }
                 $this->db->select('name');
                 $this->db->from(TABLE_SUBCATEGORY);
-                $this->db->where('id', $v->subcategory_id);
+                $this->db->where('id', $value->subcategory_id);
                 $this->db->where('status !=', '9');
                 $subquery = $this->db->get();
                 $subcat = $subquery->row();
-                print_r($subquery);
-                lq();
-                die;
+
                 $this->excel->getActiveSheet()->SetCellValue('A' . $k . '', '' . $x . '');
                 $this->excel->getActiveSheet()->SetCellValue('B' . $k . '', '' . $type . '');
                 $this->excel->getActiveSheet()->SetCellValue('C' . $k . '', '' . $value->product_name . '');
